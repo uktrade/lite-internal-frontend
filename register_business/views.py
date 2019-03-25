a7965708657255086084148129e9a2b6126f0b78
Christopher Stobart <christophers@Purple.local>
2019-03-25 15:12:40 +0000

    point post at api

--- register_business/views.py
+++ register_business/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
-
+import requests
 from register_business import forms
+from django.http import Http404
+from conf.settings import env
+import json
 
 
 def register(request):
@@ -8,3 +11,17 @@ def register(request):
         'page': forms.section1.forms[0],
     }
     return render(request, 'register_business/form.html', context)
+
+
+def submit(request):
+    response = requests.post(env("LITE_API_URL") + '/organisations/')
+    data = json.loads(response.text)
+
+    if 'errors' in data:
+        raise Http404
+
+    context = {
+        'title': 'Organisation Submitted',
+        'data': data
+    }
+    return render(request, 'register_business/registration_success.html', context)
