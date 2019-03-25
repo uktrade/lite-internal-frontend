f462e2c6c71c5049599badd07a7f6846db2cd64b
Anirban Lahiri <a.lahiri@kainos.com>
2019-03-25 14:01:29 +0000

    some updates required for interfacing with lite_api

--- organisations/views.py
+++ organisations/views.py
@@ -1,9 +1,24 @@
+import requests
+
 from django.shortcuts import render
+
+import libraries.jsondate as jsondate
+
+from conf.settings import env
 
 
 def show_orgs(request):
+    response = requests.get(env("LITE_API_URL") + '/organisations')
+
+    # print(response.text)
+
     context = {
-        'title': 'List of Registered Organizations',
+        'data': jsondate.loads(response.text),
+        'title': 'Organisations',
     }
+
+    # context = {
+    #     'title': 'List of Registered Organizations',
+    # }
     return render(request, 'organisations/index.html', context)
 
