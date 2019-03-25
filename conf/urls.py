6713864ea878cad09d63a1752f7d4af1e52facb4
Anirban Lahiri <a.lahiri@kainos.com>
2019-03-25 10:50:39 +0000

    corrected urls.py

--- conf/urls.py
+++ conf/urls.py
@@ -19,5 +19,5 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
-    path('', organisations.views.show_orgs, name='hub'),
+    path('', include('organisations.urls')),
 ]
