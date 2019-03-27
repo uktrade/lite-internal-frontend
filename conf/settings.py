4b06aa69a55b5181631dd7c11e43ee26d8064238
Christopher Stobart <christophers@Purple.local>
2019-03-27 16:25:34 +0000

    another one

--- conf/settings.py
+++ conf/settings.py
@@ -44,8 +44,9 @@ SECRET_KEY = '=(kaa@ypr5v!x(s=9^f8)o!k#84f_1v@iz31+cq_)8--@kws4b'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = json.loads(env('ALLOWED_HOSTS')) if env('ALLOWED_HOSTS') else []
 
+ALLOWED_HOSTS = json.loads(env('ALLOWED_HOSTS')) if env('ALLOWED_HOSTS') else []
+print(ALLOWED_HOSTS)
 # Application definition
 
 INSTALLED_APPS = [
