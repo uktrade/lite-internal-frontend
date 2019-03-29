4ea50fe0930435960b6e65ef05de68014107b5ac
Wisdom Ebohoin <>
2019-03-29 16:19:01 +0000

    e2e

--- automation_ui_tests/tests/test_register_business.py
+++ automation_ui_tests/tests/test_register_business.py
@@ -4,30 +4,37 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.by import By
+import datetime
 
 
-class register_business(unittest.TestCase):
+class RegisterBusinessTest(unittest.TestCase):
     @classmethod
-    def setUp(inst):
-        # get the path of ChromeDriverServer
-        dir = os.path.dirname(os.path.abspath(__file__))
-        print("check"+dir)
-        chrome_driver_path = dir + "/chromedriver"
-        # create a new Chrome session
-        inst.driver = webdriver.Chrome(chrome_driver_path)
-        inst.driver.implicitly_wait(30)
-        inst.driver.maximize_window()
+    def setUp(cls):
+
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.add_argument('--no-sandbox')
+        chrome_options.add_argument('--headless')
+        chrome_options.add_argument('--disable-gpu')
+        cls.driver = webdriver.Chrome(chrome_options=chrome_options)
+        cls.driver.implicitly_wait(10)
 
         # navigate to the application home page
-        inst.driver.get("http://localhost:7000/")
+        cls.driver.get("https://lite-internal-frontend-staging.london.cloudapps.digital/")
 
     def test_register_a_business(self):
         driver = self.driver
 
-        register_a_businessBtn = driver.find_element_by_css_selector("a[href*='/register']")
-        register_a_businessBtn.click()
+        manage_organisations_btn = driver.find_element_by_css_selector("a[href*='/organisations']")
+        manage_organisations_btn.click()
 
+        # New Organisation
+        print("Registering a new business")
+        new_organisation_btn = driver.find_element_by_css_selector("a[href*='/register']")
+        new_organisation_btn.click()
+
+        print("Entering details")
         business_name_input = driver.find_element_by_id("name")
+
         eori_number_input = driver.find_element_by_id("eori_number")
         sic_number_input = driver.find_element_by_id("sic_number")
         vat_number_input = driver.find_element_by_id("vat_number")
@@ -35,7 +42,8 @@ class register_business(unittest.TestCase):
         address_input = driver.find_element_by_id("address")
         admin_user_email_input = driver.find_element_by_id("admin_user_email")
 
-        business_name_input.send_keys("Test Business")
+        nowId = str(datetime.datetime.now())
+        business_name_input.send_keys("Test Business " + nowId)
         eori_number_input.send_keys("GB987654312000")
         sic_number_input.send_keys("73200")
         vat_number_input.send_keys("123456789")
@@ -43,31 +51,41 @@ class register_business(unittest.TestCase):
         address_input.send_keys("123 Cobalt Street")
         admin_user_email_input.send_keys("joe@bloss.com")
 
+        print("Submitting...")
         submit = driver.find_element_by_xpath("//*[@action='submit']")
         submit.click()
 
         registration_complete_message = driver.find_element_by_tag_name("h1").text
-        business_id = driver.find_element_by_css_selector(".govuk-panel--confirmation div").text[-36:]
+        business_id = nowId
         assert "Registration complete" == registration_complete_message
+        print("Submitted")
 
-        driver.get("http://localhost:7000/")
+        driver.get("https://lite-internal-frontend-staging.london.cloudapps.digital/")
 
         # verify application is in organisations list
         show_registered_organisations = driver.find_element_by_css_selector("a[href*='/organisations']")
         show_registered_organisations.click()
 
-        self.assertTrue(self.is_element_present(By.XPATH,".//td/a[contains(@href,'" + business_id + "')]"))
+        self.assertTrue(self.is_element_present(By.XPATH,"//*[text()[contains(.,'"+ nowId +"')]]"))
 
     def test_cancel_register_a_business(self):
         driver = self.driver
+        manage_organisations_btn = driver.find_element_by_css_selector("a[href*='/organisations']")
+        manage_organisations_btn.click()
 
-        register_a_businessBtn = driver.find_element_by_css_selector("a[href*='/register']")
-        register_a_businessBtn.click()
+        # New Organisation
+        print("Registering a new business")
+        new_organisation_btn = driver.find_element_by_css_selector("a[href*='/register']")
+        new_organisation_btn.click()
 
+        print("Cancelling...")
         cancel_btn = driver.find_element_by_css_selector("a[href*='/organisations']")
         cancel_btn.click()
 
-        assert "Organisations" in driver.title
+        title = driver.title
+        assert "Organisations" in title
+
+        print("Cancelled")
 
     @classmethod
     def tearDown(inst):
