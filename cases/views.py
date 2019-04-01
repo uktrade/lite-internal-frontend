b4e84c16dc74f5ad3db09b0aebdb77d0075d09ec
Jan Faracik <j.faracik@kainos.com>
2019-04-01 19:48:04 +0100

    You can now view queues and cases

--- cases/views.py
+++ cases/views.py
@@ -8,9 +8,18 @@ from conf.settings import env
 
 
 def cases(request):
-    response = requests.get(env("LITE_API_URL") + '/cases')
+    queue_id = request.GET.get('queue')
+
+    if not queue_id:
+        queue_id = '00000000-0000-0000-0000-000000000000'
+
+    queues = requests.get(env("LITE_API_URL") + '/queues/')
+    response = requests.get(env("LITE_API_URL") + '/queues/' + queue_id + '/')
+
     context = {
-        #'data': jsondate.loads(response.text),
+        'queues': jsondate.loads(queues.text),
+        'queue_id': queue_id,
+        'data': jsondate.loads(response.text),
         'title': 'Cases',
     }
     return render(request, 'cases/index.html', context)
