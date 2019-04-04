e366308e55f213a72021c9d14259bfca73bab21a
Jan Faracik <j.faracik@kainos.com>
2019-04-04 10:43:24 +0100

    Made view more consistent

--- cases/views.py
+++ cases/views.py
@@ -8,27 +8,27 @@ from conf.settings import env
 def index(request):
     queue_id = request.GET.get('queue')
 
+    # If a queue id is not provided, use the default queue
     if not queue_id:
         queue_id = '00000000-0000-0000-0000-000000000000'
 
-    queues = requests.get(env("LITE_API_URL") + '/queues/')
-    response = requests.get(env("LITE_API_URL") + '/queues/' + queue_id + '/')
+    queues = requests.get(env("LITE_API_URL") + '/queues/').json()
+    response = requests.get(env("LITE_API_URL") + '/queues/' + queue_id + '/').json()
 
     context = {
-        'queues': queues.json(),
+        'queues': queues,
         'queue_id': queue_id,
-        'data': response.json(),
-        'title': response.json()['queue']['name'],
+        'data': response,
+        'title': response.get('queue').get('name'),
     }
     return render(request, 'cases/index.html', context)
 
 
 def case(request, pk):
-    response = requests.get(env("LITE_API_URL") + '/cases/' + str(pk) + '/')
-    data = response.json()
+    response = requests.get(env("LITE_API_URL") + '/cases/' + str(pk) + '/').json()
 
     context = {
-        'data': data,
-        'title': data.get("case").get("application").get("name"),
+        'data': response,
+        'title': response.get('case').get('application').get('name'),
     }
     return render(request, 'cases/case.html', context)
