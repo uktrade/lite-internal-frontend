8badc25be55e2b3a97a924bff7997d90b713fe8b
Jan Faracik <j.faracik@kainos.com>
2019-04-17 16:55:26 +0100

    Removed npm, added submodules

--- core/apps.py
+++ core/apps.py
@@ -9,5 +9,5 @@ class CoreConfig(AppConfig):
     name = 'core'
 
     def ready(self):
-        with open('strings.json') as json_file:
+        with open('lite-content/lite-internal-frontend/strings.json') as json_file:
             strings.constants = json.load(json_file)
