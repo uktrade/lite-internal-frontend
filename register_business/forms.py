c1916682e94a338d6ec34bebe6368d237a4e2e38
Jan Faracik <j.faracik@kainos.com>
2019-05-03 16:00:46 +0100

    Now submits

--- register_business/forms.py
+++ register_business/forms.py
@@ -1,66 +1,77 @@
-from libraries.forms.components import Section, Form, Question, InputType, Button, HiddenField, HeadingStyle, Heading, \
+from libraries.forms.components import Section, Form, Question, InputType, Button, HeadingStyle, Heading, \
     HelpSection
 
 register_business_forms = Section('', '', [
-    Form('Register a Business', 'Part 1 of 3', [
-        Question(title='What\'s the business name?',
-                 description='',
-                 input_type=InputType.INPUT,
-                 name='business.name'),
-        Question(title='European Union registration and identification number (EORI)',
-                 description='',
-                 input_type=InputType.INPUT,
-                 name='business.eori_number'),
-        Question(title='Standard Industrial Classification Number (SIC)',
-                 description='Classifies industries by a four-digit code.',
-                 input_type=InputType.INPUT,
-                 name='business.sic_number'),
-        Question(title='UK VAT number',
-                 description='9 digits long, with the first two letters indicating the'
-                             ' country code of the registered business.',
-                 input_type=InputType.INPUT,
-                 name='business.vat_number'),
-        Question(title='Company registration number (CRN)',
-                 description='8 numbers, or 2 letters followed by 6 numbers.',
-                 input_type=InputType.INPUT,
-                 name='business.registration_number'),
-    ], buttons=[
-        Button('Save and continue', '')
-    ]),
-    Form('Create a default site for this organisation', 'Part 2 of 3', [
-        Question(title='Name of site',
-                 description='',
-                 input_type=InputType.INPUT,
-                 name='site.name'),
-        Heading('Address', HeadingStyle.M),
-        Question(title='Address Line 1',
-                 description='',
-                 input_type=InputType.INPUT,
-                 name='site.address_line_1'),
-        Question(title='Address Line 2',
-                 description='',
-                 input_type=InputType.INPUT,
-                 name='site.address_line_2',
-                 optional=True),
-        Question(title='Post code',
-                 description='',
-                 input_type=InputType.INPUT,
-                 name='site.post_code'),
-        Question(title='City',
-                 description='',
-                 input_type=InputType.INPUT,
-                 name='site.city'),
-        Question(title='State',
-                 description='',
-                 input_type=InputType.INPUT,
-                 name='site.state'),
-        Question(title='Country',
-                 description='',
-                 input_type=InputType.INPUT,
-                 name='site.country'),
-    ], buttons=[
-        Button('Save and continue', '')
-    ]),
+
+    Form(title='Register a Business',
+         description='Part 1 of 3',
+         questions=[
+             Question(title='What\'s the business name?',
+                      description='',
+                      input_type=InputType.INPUT,
+                      name='organisation.name'),
+             Question(title='European Union registration and identification number (EORI)',
+                      description='',
+                      input_type=InputType.INPUT,
+                      name='organisation.eori_number'),
+             Question(title='Standard Industrial Classification Number (SIC)',
+                      description='Classifies industries by a four-digit code.',
+                      input_type=InputType.INPUT,
+                      name='organisation.sic_number'),
+             Question(title='UK VAT number',
+                      description='9 digits long, with the first two letters indicating the'
+                                  ' country code of the registered business.',
+                      input_type=InputType.INPUT,
+                      name='organisation.vat_number'),
+             Question(title='Company registration number (CRN)',
+                      description='8 numbers, or 2 letters followed by 6 numbers.',
+                      input_type=InputType.INPUT,
+                      name='organisation.registration_number'),
+         ],
+         buttons=[
+             Button('Save and continue', '')
+         ],
+         prefix='organisation'
+         ),
+
+    Form(title='Create a default site for this organisation',
+         description='Part 2 of 3',
+         questions=[
+             Question(title='Name of site',
+                      description='',
+                      input_type=InputType.INPUT,
+                      name='site.name'),
+             Heading('Address', HeadingStyle.M),
+             Question(title='Address Line 1',
+                      description='',
+                      input_type=InputType.INPUT,
+                      name='site.address_line_1'),
+             Question(title='Address Line 2',
+                      description='',
+                      input_type=InputType.INPUT,
+                      name='site.address_line_2',
+                      optional=True),
+             Question(title='Post code',
+                      description='',
+                      input_type=InputType.INPUT,
+                      name='site.post_code'),
+             Question(title='City',
+                      description='',
+                      input_type=InputType.INPUT,
+                      name='site.city'),
+             Question(title='State',
+                      description='',
+                      input_type=InputType.INPUT,
+                      name='site.state'),
+             Question(title='Country',
+                      description='',
+                      input_type=InputType.INPUT,
+                      name='site.country'),
+         ], buttons=[
+            Button('Save and continue', '')
+        ],
+         prefix='site'
+         ),
     Form('Create an admin for this organisation', 'Part 3 of 3', [
         Question(title='Email Address',
                  description='',
@@ -86,5 +97,7 @@ register_business_forms = Section('', '', [
         Button('Submit', '')
     ], helpers=[
         HelpSection('Help', 'This will be the default user for this organisation.')
-    ]),
+    ],
+         prefix='user'
+         ),
 ])
--- register_business/views.py
+++ register_business/views.py
@@ -3,8 +3,9 @@ import json
 from django.shortcuts import render, redirect
 from django.views.generic import TemplateView
 
+from conf.client import post
 from libraries.forms.components import HiddenField
-from libraries.forms.helpers import get_next_form_after_pk, nest_data
+from libraries.forms.helpers import get_next_form_after_pk, nest_data, get_form_by_pk
 from register_business import forms
 
 
@@ -20,16 +21,41 @@ class RegisterBusiness(TemplateView):
         data = request.POST.copy()
 
         # Get the next form based off form_pk
+        current_form = get_form_by_pk(data.get('form_pk'), forms.register_business_forms)
         form = get_next_form_after_pk(data.get('form_pk'), forms.register_business_forms)
 
         # Remove form_pk and CSRF from POST data as the new form will replace them
         del data['form_pk']
         del data['csrfmiddlewaretoken']
 
+        # Post the data to the validator and check for errors
+        nested_data = nest_data(data)
+
+        validated_data = post(request, '/organisations/validate/', nested_data).json()
+
+        if 'errors' in validated_data:
+            context = {
+                'page': current_form,
+                'title': current_form.title,
+                'errors': validated_data['errors'].get(current_form.prefix),
+                'data': data,
+            }
+            return render(request, 'form.html', context)
+
         # If there aren't any forms left to go through, submit all the data
         if form is None:
-            print(json.dumps(nest_data(data)))
-            return redirect('/')
+            validated_data = post(request, '/organisations/', nested_data).json()
+
+            if 'errors' in validated_data:
+                context = {
+                    'page': current_form,
+                    'title': current_form.title,
+                    'errors': validated_data['errors'].get(current_form.prefix),
+                    'data': data,
+                }
+                return render(request, 'form.html', context)
+
+            return redirect('/organisations/')
 
         # Add existing post data to new form as hidden fields
         for key, value in data.items():
