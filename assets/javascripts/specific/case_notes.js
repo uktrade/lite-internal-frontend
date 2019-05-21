d25ab0e282e3e89620d153c7383baea4086beb17
Jan Faracik <j.faracik@kainos.com>
2019-05-21 17:39:16 +0100

    Added constants for case note validation

--- assets/javascripts/specific/case_notes.js
+++ assets/javascripts/specific/case_notes.js
@@ -1,3 +1,7 @@
+const MIN_LENGTH = 1;
+const VISIBLE_LENGTH = 1200;
+const MAX_LENGTH = 2200;
+
 $(".lite-expandable-textarea-controls").addClass("lite-expandable-textarea-controls--hidden");
 $(".case_note-warning").addClass("govuk-hint--hidden");
 
@@ -13,21 +17,21 @@ $("#case_note").on('input propertychange paste', function() {
 		$(".lite-expandable-textarea-controls").addClass("lite-expandable-textarea-controls--hidden");
 	}
 
-	if ($(this).val().length > 1200) {
-		$("#case_note-warning").text("You have " + (2200 - $(this).val().length) + " character" + pluralize(2200 - $(this).val().length) + " remaining");
+	if ($(this).val().length > VISIBLE_LENGTH) {
+		$("#case_note-warning").text("You have " + (MAX_LENGTH - $(this).val().length) + " character" + pluralize(MAX_LENGTH - $(this).val().length) + " remaining");
 	} else {
-		$("#case_note-warning").text("You can enter up to 2200 characters");
+		$("#case_note-warning").text("You can enter up to " + MAX_LENGTH + " characters");
 	}
 
-	if ($(this).val().length <= 1) {
+	if ($(this).val().length <= MIN_LENGTH) {
 		$("#button-post-note").addClass("govuk-button--disabled");
 		$("#button-post-note").attr("disabled", true);
 	}
 
-	if ($(this).val().length > 2200) {
+	if ($(this).val().length > MAX_LENGTH) {
 		$("#case_note-warning").removeClass("govuk-hint");
 		$("#case_note-warning").addClass("govuk-error-message");
-		$("#case_note-warning").text("You have " + ($(this).val().length - 2200) + " character" + pluralize($(this).val().length - 2200) + " too many");
+		$("#case_note-warning").text("You have " + ($(this).val().length - MAX_LENGTH) + " character" + pluralize($(this).val().length - MAX_LENGTH) + " too many");
 		$("#button-post-note").addClass("govuk-button--disabled");
 		$("#button-post-note").attr("disabled", true);
 		$(".lite-expandable-textarea").addClass("lite-expandable-textarea--warning");
@@ -35,7 +39,7 @@ $("#case_note").on('input propertychange paste', function() {
 		$("#case_note-warning").removeClass("govuk-error-message");
 		$("#case_note-warning").addClass("govuk-hint");
 		$(".lite-expandable-textarea").removeClass("lite-expandable-textarea--warning");
-		if ($(this).val().length > 1) {
+		if ($(this).val().length > MIN_LENGTH) {
 			$("#button-post-note").removeClass("govuk-button--disabled");
 			$("#button-post-note").attr("disabled", false);
 		}
