467dc375f269b0d7a7a30de6df53de5f8fb0b5e8
Jan Faracik <j.faracik@kainos.com>
2019-05-24 11:27:32 +0100

    Added applicant

--- cases/urls.py
+++ cases/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     # ex: /
     path('', views.index, name='cases'),
     # ex: /43a88949-5db9-4334-b0cc-044e91827451
-    path('<uuid:pk>', views.ViewCase.as_view(), name='case'),
+    path('<uuid:pk>/', views.ViewCase.as_view(), name='case'),
     # ex: /43a88949-5db9-4334-b0cc-044e91827451/manage
     path('<uuid:pk>/manage', views.ManageCase.as_view(), name='manage'),
     # ex: /43a88949-5db9-4334-b0cc-044e91827451/decide
