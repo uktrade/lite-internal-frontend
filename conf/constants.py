594da59033b541d796c1c9a98a76313468083b75
Jan Faracik <j.faracik@kainos.com>
2019-05-29 11:50:58 +0100

    Added denial reasons

--- conf/constants.py
+++ conf/constants.py
@@ -5,3 +5,7 @@ ORGANISATIONS_URL = '/organisations/'
 CASE_URL = '/cases/'
 CASE_NOTES_URL = '/case_notes/'
 SITES_URL = '/sites/'
+
+# Static URLs
+STATIC_URL = '/static/'
+DENIAL_REASONS_URL = STATIC_URL + 'denial-reasons/'
---
+++ core/services.py
@@ -0,0 +1,15 @@
+from conf.client import get
+from conf.constants import DENIAL_REASONS_URL
+from libraries.forms.components import Option
+
+
+def get_denial_reasons(request):
+    data = get(request, DENIAL_REASONS_URL).json()
+    converted = []
+
+    for denial_reason in data.get('denial_reasons').items():
+        converted.append(
+           Option(denial_reason['id'], denial_reason['id'])
+        )
+
+    return converted
