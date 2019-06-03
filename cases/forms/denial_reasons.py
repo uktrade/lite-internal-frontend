a7ff81a78b69daef3da830065e74555e70b927af
Christopher Stobart <christophers@Purple.local>
2019-06-03 11:16:16 +0100

    content changes

--- cases/forms/denial_reasons.py
+++ cases/forms/denial_reasons.py
@@ -4,7 +4,7 @@ from libraries.forms.components import Question, Form, InputType
 
 def denial_reasons_form():
     form = Form('Why do you want to deny this application?', 'Select all that apply.', get_denial_reasons(None),
-                default_button_name='Submit denial')
+                default_button_name='Submit')
 
     form.questions.append(Question('Add any additional information to support your denial',
                                    '',
