05ebf6e72d00c6e48e0d7f267e4a4e88d337d47a
Christopher Stobart <christophers@Purple.local>
2019-06-13 16:38:18 +0100

    Update forms.py

--- users/forms.py
+++ users/forms.py
@@ -1,4 +1,5 @@
-from libraries.forms.components import Question, Form, InputType
+from libraries.forms.components import Question, Form, InputType, ArrayQuestion
+from teams.services import get_teams
 
 form = Form(title='Add User', description='', caption='', questions=[
     Question(title='First name',
@@ -13,6 +14,11 @@ form = Form(title='Add User', description='', caption='', questions=[
              description='',
              input_type=InputType.INPUT,
              name='email'),
+    ArrayQuestion(title='Team',
+                  description='',
+                  input_type=InputType.AUTOCOMPLETE,
+                  name='team',
+                  data=get_teams(request)),
 ])
 
 edit_form = Form(title='Edit User', description='', caption='', questions=[
