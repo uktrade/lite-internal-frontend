76e7b2caea038e91c0affbc023ebc459c9fa472a
Jan Faracik <j.faracik@kainos.com>
2019-06-18 16:22:14 +0100

    Moved content to lite-content

--- cases/forms/move_case.py
+++ cases/forms/move_case.py
@@ -1,10 +1,11 @@
+from core.builtins.custom_tags import get_string
 from core.services import get_queues
 from libraries.forms.components import Form, ArrayQuestion, InputType
 
 
 def move_case_form(request):
-    return Form('Where do you want to move this case?',
-                'Select all queues that apply.',
+    return Form(get_string('cases.manage.move_case.title'),
+                get_string('cases.manage.move_case.description'),
                 [
                     ArrayQuestion('', '', InputType.CHECKBOXES, 'queues', get_queues(request, True))
                 ],
