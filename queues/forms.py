ca78a2f5e3323167820b6120439560300e4bf888
Anirban Lahiri <a.lahiri@kainos.com>
2019-06-20 08:13:41 +0100

    Updates to use content strings

--- queues/forms.py
+++ queues/forms.py
@@ -1,14 +1,15 @@
+from core.builtins.custom_tags import get_string
 from libraries.forms.components import Question, Form, InputType
 
-form = Form(title='Add Queue', description='', caption='', questions=[
-    Question(title='Name',
+form = Form(title=get_string('queues.queue_add.page_heading'), description='', caption='', questions=[
+    Question(title=get_string('queues.queue_add.question_title'),
              description='',
              input_type=InputType.INPUT,
              name='name'),
 ])
 
-edit_form = Form(title='Edit Queue', description='', caption='', questions=[
-    Question(title='Name',
+edit_form = Form(title=get_string('queues.queue_edit.page_heading'), description='', caption='', questions=[
+    Question(title=get_string('queues.queue_edit.question_title'),
              description='',
              input_type=InputType.INPUT,
              name='name'),
