21a8c9bd08529f09c0d81055bd06c22a976f3ca6
Jan Faracik <j.faracik@kainos.com>
2019-07-03 14:37:45 +0100

    Delete attach_documents.py

--- cases/forms/attach_documents.py
+++
@@ -1,9 +0,0 @@
-from libraries.forms.components import Form, MultiFileUpload
-
-
-def attach_documents_form():
-    return Form('Attach Documents',
-                'Attach all relevant documents.',
-                [
-                    MultiFileUpload('documents')
-                ])
