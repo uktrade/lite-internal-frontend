29c4d51fe5f880957a1beee3f2f133d4293dbc0c
cstobart <c.stobart@kainos.com>
2019-07-15 14:51:25 +0100

    Update roles_pages.py

--- ui_automation_tests/pages/roles_pages.py
+++ ui_automation_tests/pages/roles_pages.py
@@ -1,1 +1,15 @@
+from selenium.webdriver.support.select import Select
+
+
 class RolesPages():
+
+    def enter_role_name(self, text):
+        self.driver.find_element_by_id(self.add_role_text_field).clear()
+        self.driver.find_element_by_id(self.add_role_text_field).send_keys(text)
+
+    def select_permissions(self, value):
+        select = Select(self.driver.find_element_by_id('permissions'))
+        select.select_by_visible_text(value)
+
+    def click_add_a_role_button(self):
+        self.driver.find_element_by_css_selector(self.add_role_button).click()
