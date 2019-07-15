4961a73539c325f2ab40209d94c5882c924e25e0
Jan Faracik <j.faracik@kainos.com>
2019-07-15 16:21:14 +0100

    Update forms.py

--- flags/forms.py
+++ flags/forms.py
@@ -1,36 +1,31 @@
-from core.builtins.custom_tags import get_string
-from libraries.forms.components import Question, Form, InputType, ArrayQuestion, HelpSection, Option
+from libraries.forms.components import Question, Form, InputType, Option, Select
 
 
 def add_flag_form():
     return Form(title='Add Flag', description='', questions=[
-            Question(title='Name',
-                     description='',
-                     input_type=InputType.INPUT,
-                     name='name'),
-            ArrayQuestion(title='Level',
-                          description='',
-                          input_type=InputType.SELECT,
-                          name='level',
-                          data=[Option('Case', 'Case'),
-                                Option('Organisation', 'Organisation'),
-                                Option('Destination', 'Destination'),
-                                Option('Good', 'Good')])
-        ])
+        Question(title='Name',
+                 description='',
+                 input_type=InputType.INPUT,
+                 name='name'),
+        Select(name='level',
+               options=[Option('Case', 'Case'),
+                        Option('Organisation', 'Organisation'),
+                        Option('Destination', 'Destination'),
+                        Option('Good', 'Good')],
+               title='Level', )
+    ])
 
 
 def edit_flag_form():
     return Form(title='Edit Flag', description='', caption='', questions=[
-            Question(title='Name',
-                     description='',
-                     input_type=InputType.INPUT,
-                     name='name'),
-            ArrayQuestion(title='Level',
-                          description='',
-                          input_type=InputType.SELECT,
-                          name='level',
-                          data=[Option('Case', 'Case'),
-                                Option('Organisation', 'Organisation'),
-                                Option('Destination', 'Destination'),
-                                Option('Good', 'Good')])
-        ])
+        Question(title='Name',
+                 description='',
+                 input_type=InputType.INPUT,
+                 name='name'),
+        Select(name='level',
+               options=[Option('Case', 'Case'),
+                        Option('Organisation', 'Organisation'),
+                        Option('Destination', 'Destination'),
+                        Option('Good', 'Good')],
+               title='Level', )
+    ])
