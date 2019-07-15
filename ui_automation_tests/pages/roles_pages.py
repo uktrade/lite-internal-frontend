3f42ea2c8eac910c81ca1ed4f077fbe072550b3c
cstobart <c.stobart@kainos.com>
2019-07-15 14:53:05 +0100

    Update roles_pages.py

--- ui_automation_tests/pages/roles_pages.py
+++ ui_automation_tests/pages/roles_pages.py
@@ -3,6 +3,11 @@ from selenium.webdriver.support.select import Select
 
 class RolesPages():
 
+    def __init__(self, driver):
+        self.driver = driver
+        self.add_role_text_field = "name"
+        self.add_role_button = ".govuk-button[href*='roles/add']"
+
     def enter_role_name(self, text):
         self.driver.find_element_by_id(self.add_role_text_field).clear()
         self.driver.find_element_by_id(self.add_role_text_field).send_keys(text)
