7a9854e4eb88b927acde31c94c66f434495f3632
Jan Faracik <j.faracik@kainos.com>
2019-07-15 14:02:04 +0100

    Added role name

--- users/forms/users.py
+++ users/forms/users.py
@@ -1,30 +1,37 @@
 from core.builtins.custom_tags import get_string
-from libraries.forms.components import Question, Form, InputType, ArrayQuestion, HelpSection
+from libraries.forms.components import Question, Form, InputType, ArrayQuestion, Select
+from users.services import get_roles
 
 
-def add_user_form(teams):
+def add_user_form(request, teams):
     return Form(title=get_string('users.invite'), description='', questions=[
-            Question(title='What\'s the user\'s email?',
-                     description='',
-                     input_type=InputType.INPUT,
-                     name='email'),
-            ArrayQuestion(title='What team will the user belong to?',
-                          description='',
-                          input_type=InputType.SELECT,
-                          name='team',
-                          data=teams),
-        ])
+        Question(title='What\'s the user\'s email?',
+                 description='',
+                 input_type=InputType.INPUT,
+                 name='email'),
+        ArrayQuestion(title='What team will the user belong to?',
+                      description='',
+                      input_type=InputType.SELECT,
+                      name='team',
+                      data=teams),
+        Select(name='role',
+               options=get_roles(request, True),
+               title='What role should this user have?'),
+    ])
 
 
-def edit_user_form(teams):
+def edit_user_form(request, teams):
     return Form(title='Edit User', description='', caption='', questions=[
-            Question(title='Email',
-                     description='',
-                     input_type=InputType.INPUT,
-                     name='email'),
-            ArrayQuestion(title='Team',
-                          description='',
-                          input_type=InputType.SELECT,
-                          name='team',
-                          data=teams),
-        ])
+        Question(title='Email',
+                 description='',
+                 input_type=InputType.INPUT,
+                 name='email'),
+        ArrayQuestion(title='Team',
+                      description='',
+                      input_type=InputType.SELECT,
+                      name='team',
+                      data=teams),
+        Select(name='role',
+               options=get_roles(request, True),
+               title='What role should this user have?'),
+    ])
