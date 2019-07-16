e77a60e865e0c86e07ed784de50e7c59fb30a3aa
Jan Faracik <j.faracik@kainos.com>
2019-07-16 09:14:54 +0100

    Update forms.py

--- flags/forms.py
+++ flags/forms.py
@@ -1,31 +1,27 @@
 from libraries.forms.components import Question, Form, InputType, Option, Select
 
+_name = Question(title='Name',
+                 description='',
+                 input_type=InputType.INPUT,
+                 name='name')
+
+_level = Select(name='level',
+                options=[Option('Case', 'Case'),
+                         Option('Organisation', 'Organisation'),
+                         Option('Destination', 'Destination'),
+                         Option('Good', 'Good')],
+                title='Level')
+
 
 def add_flag_form():
     return Form(title='Add Flag', description='', questions=[
-        Question(title='Name',
-                 description='',
-                 input_type=InputType.INPUT,
-                 name='name'),
-        Select(name='level',
-               options=[Option('Case', 'Case'),
-                        Option('Organisation', 'Organisation'),
-                        Option('Destination', 'Destination'),
-                        Option('Good', 'Good')],
-               title='Level', )
+        _name,
+        _level,
     ])
 
 
 def edit_flag_form():
-    return Form(title='Edit Flag', description='', caption='', questions=[
-        Question(title='Name',
-                 description='',
-                 input_type=InputType.INPUT,
-                 name='name'),
-        Select(name='level',
-               options=[Option('Case', 'Case'),
-                        Option('Organisation', 'Organisation'),
-                        Option('Destination', 'Destination'),
-                        Option('Good', 'Good')],
-               title='Level', )
+    return Form(title='Edit Flag', description='', questions=[
+        _name,
+        _level,
     ])
