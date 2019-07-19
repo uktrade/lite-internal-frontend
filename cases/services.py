631009438fca7b7d405eec12ebe39b16d937aefd
Jan Faracik <43062514+jfaracik@users.noreply.github.com>
2019-07-19 09:26:44 +0100

    feat: LT-945 - Attach documents to cases (#56)
    * s3 chunk uploader
    * storing metadata

--- cases/services.py
+++ cases/services.py
@@ -1,5 +1,5 @@
-from conf.client import post, get, put
-from conf.constants import CASE_URL, CASE_NOTES_URL, APPLICATIONS_URL, ACTIVITY_URL, CLC_QUERIES_URL
+from conf.client import post, get, put, delete
+from conf.constants import CASE_URL, CASE_NOTES_URL, APPLICATIONS_URL, ACTIVITY_URL, CLC_QUERIES_URL, DOCUMENTS_URL
 
 
 def get_case(request, pk):
@@ -38,4 +38,25 @@ def post_case_notes(request, pk, json):
 # Activity
 def get_activity(request, pk):
     data = get(request, CASE_URL + pk + ACTIVITY_URL + '?fields=status')
+    return data.json(), data.status_code
+
+
+# Case Documents
+def get_case_document(request, pk, s3_key):
+    data = get(request, CASE_URL + pk + DOCUMENTS_URL + s3_key)
+    return data.json(), data.status_code
+
+
+def get_case_documents(request, pk):
+    data = get(request, CASE_URL + pk + DOCUMENTS_URL)
+    return data.json(), data.status_code
+
+
+def post_case_documents(request, pk, json):
+    data = post(request, CASE_URL + pk + DOCUMENTS_URL, json)
+    return data.json(), data.status_code
+
+
+def delete_case_document(request, pk, s3_key):
+    data = delete(request, CASE_URL + pk + DOCUMENTS_URL + s3_key)
     return data.json(), data.status_code
