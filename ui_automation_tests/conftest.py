bca709c46fce762e43c1e423ed00c08eea507586
Nick Philpott <nicholasp@kainos.com>
2019-07-24 10:22:40 +0100

    Update conftest.py

--- ui_automation_tests/conftest.py
+++ ui_automation_tests/conftest.py
@@ -35,8 +35,8 @@ def pytest_addoption(parser):
                      default="https://exporter.lite.service." + env + ".uktrade.io/", help="url")
     parser.addoption("--internal_url", action="store",
                      default="https://internal.lite.service." + env + ".uktrade.io/", help="url")
-    #parser.addoption("--exporter_url", action="store", default="http://localhost:8300", help="url")
-    #parser.addoption("--internal_url", action="store", default="http://localhost:8200", help="url")
+    # parser.addoption("--exporter_url", action="store", default="http://localhost:8300", help="url")
+    # parser.addoption("--internal_url", action="store", default="http://localhost:8200", help="url")
     parser.addoption("--sso_sign_in_url", action="store", default="https://sso.trade.uat.uktrade.io/login/", help="url")
 
 
