d7c21d156ba15459fdec3eae425bafc2c8f7dc58
Jan Faracik <j.faracik@kainos.com>
2019-07-31 12:43:39 +0100

    cleanup

--- picklists/views.py
+++ picklists/views.py
@@ -2,10 +2,11 @@ from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from django.views.generic import TemplateView
 
-from flags.forms import edit_flag_form
 from libraries.forms.generators import form_page
 from picklists.forms import add_picklist_item_form, edit_picklist_item_form
 from picklists.services import get_picklists, get_picklist_item, post_picklist_item, put_picklist_item
+from teams.services import get_team
+from users.services import get_gov_user
 
 
 class Picklists(TemplateView):
@@ -16,11 +17,14 @@ class Picklists(TemplateView):
         if not picklist_type:
             return redirect(reverse_lazy('picklists:picklists') + '?type=proviso')
 
+        user, status_code = get_gov_user(request)
+        team, status_code = get_team(request, user['user']['team']['id'])
         # Get picklist items depending on the type given
         picklist_items, status_code = get_picklists(request, picklist_type)
 
         context = {
-            'title': 'Picklists',
+            'title': 'Picklists - ' + team['team']['name'],
+            'team': team['team'],
             'picklist_items': picklist_items['picklist_items'],
         }
         return render(request, 'teams/picklist.html', context)
--- teams/urls.py
+++ teams/urls.py
@@ -1,17 +1,18 @@
-from django.urls import path, include
+from django.urls import path
 
 from teams import views
 
 app_name = 'teams'
 urlpatterns = [
+    # ex: /team/
+    path('', views.Team.as_view(), name='team'),
+
     # ex: /teams/
-    path('', views.TeamsList.as_view(), name='teams'),
+    path('s/', views.TeamsList.as_view(), name='teams'),
     # ex: /teams/add/
-    path('add', views.AddTeam.as_view(), name='add'),
+    path('s/add', views.AddTeam.as_view(), name='add'),
     # ex: /teams/<uuid:pk>/
-    path('<uuid:pk>', views.TeamDetail.as_view(), name='team'),
+    path('s/<uuid:pk>', views.TeamDetail.as_view(), name='team'),
     # ex: /teams/<uuid:pk>/edit/
-    path('<uuid:pk>/edit', views.EditTeam.as_view(), name='edit'),
-    # ex: /teams/<uuid:pk>/picklists/
-    path('<uuid:pk>/picklists/', include('teams.picklists.urls')),
+    path('s/<uuid:pk>/edit', views.EditTeam.as_view(), name='edit'),
 ]
