b1d9e3829440e56f84e0d600373deb07abcf6fc7
AntonH <a.hughes@kainos.com>
2019-07-31 15:31:55 +0100

    front end complete 1077

--- picklists/services.py
+++ picklists/services.py
@@ -7,11 +7,6 @@ def get_picklists(request, picklist_type):
     return data.json(), data.status_code
 
 
-# def get_flags_case_level_for_team(request):
-#     data = get(request, FLAGS_CASE_LEVEL_FOR_TEAM)
-#     return data.json(), data.status_code
-
-
 def post_picklist_item(request, json):
     data = post(request, PICKLIST_URL, json)
     return data.json(), data.status_code
