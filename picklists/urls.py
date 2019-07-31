53c49ecefdbb6c8d3cea617bb9c21434d85736ee
Jan Faracik <j.faracik@kainos.com>
2019-07-31 09:19:18 +0100

    bla

--- picklists/urls.py
+++ picklists/urls.py
@@ -5,15 +5,11 @@ from picklists import views
 app_name = 'picklists'
 urlpatterns = [
     # ex: /picklists/ - View all active picklists
-    path('', views.Picklist.as_view(), name='picklists'),
-    # # ex: /all/ - view all flags
-    # path('<str:status>/', views.FlagsList.as_view(), name='flags'),
+    path('', views.Picklists.as_view(), name='picklists'),
     # ex: /picklist/add/ -  add a new picklist item
-    # path('add', views.PickListItem.as_view(), name='add'),
-    # # ex: /picklist-item/43a88949-5db9-4334-b0cc-044e91827451/ - Go to a flags's detail
-    # path('<uuid:pk>/profile/', views.ViewPicklistItem.as_view(), name='picklist_item'),
-    # # ex: /picklist-item/<uuid:pk>/edit - edit a picklist item
-    # path('<uuid:pk>/edit/', views.EditFlag.as_view(), name='edit'),
-    # # ex: /picklist-item/<uuid:pk>/edit/deactivate - deactivate or reactivate a picklist-item
-    # path('<uuid:pk>/edit/<str:status>/', views.ChangePicklistItemStatus.as_view(), name='change_picklist_item_status'),
+    path('add/', views.AddPicklistItem.as_view(), name='add'),
+    # # ex: /picklists/<uuid:pk>/ - Go to a flags's detail
+    # path('<uuid:pk>/', views.PicklistItem.as_view(), name='picklist_item'),
+    # # ex: /picklists/<uuid:pk>/edit/ - edit a picklist item
+    # path('<uuid:pk>/edit/', views.EditPicklistItem.as_view(), name='edit'),
 ]
--- picklists/views.py
+++ picklists/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 
+from libraries.forms.generators import form_page
+from picklists.forms import add_picklist_item_form
 from picklists.services import get_picklists
 from users.services import get_gov_user
 
 
-class Picklist(TemplateView):
+class Picklists(TemplateView):
 
     def get(self, request, **kwargs):
         # data, status_code = get_picklists(request)
@@ -14,31 +16,55 @@ class Picklist(TemplateView):
         context = {
             'title': 'Pick List',
             'picklist_items': [
-                    {
-                        'id': '123',
-                        'team': '123',
-                        'name': 'my pick list item!!',
-                        'text': '590000sdifjkn dskjfnskd,f jksdn jkfgndcjkaws vdf',
-                        'type': 'Provisio',
-                        'status': 'Activated'
-                    }
-                ],
+                {
+                    'id': '123',
+                    'team': '123',
+                    'name': 'my pick list item!!',
+                    'text': '590000sdifjkn dskjfnskd,f jksdn jkfgndcjkaws vdf',
+                    'type': 'Provisio',
+                    'status': 'Activated'
+                },
+                {
+                    'id': '123',
+                    'team': '123',
+                    'name': 'my pick list item!!',
+                    'text': '590000sdifjkn dskjfnskd,f jksdn jkfgndcjkaws vdf',
+                    'type': 'Provisio',
+                    'status': 'Activated'
+                },
+                {
+                    'id': '123',
+                    'team': '123',
+                    'name': 'my pick list item!!',
+                    'text': '590000sdifjkn dskjfnskd,f jksdn jkfgndcjkaws vdf',
+                    'type': 'Provisio',
+                    'status': 'Activated'
+                },
+                {
+                    'id': '123',
+                    'team': '123',
+                    'name': 'my pick list item!!',
+                    'text': '590000sdifjkn dskjfnskd,f jksdn jkfgndcjkaws vdf',
+                    'type': 'Provisio',
+                    'status': 'Activated'
+                }
+            ],
         }
         return render(request, 'teams/picklist.html', context)
 
 
-# class AddFlag(TemplateView):
-#     def get(self, request, **kwargs):
-#         return form_page(request, add_flag_form())
-#
-#     def post(self, request, **kwargs):
-#         response, status_code = post_flags(request, request.POST)
-#         if status_code != 201:
-#             return form_page(request, add_flag_form(), data=request.POST, errors=response.get('errors'))
-#
-#         return redirect(reverse_lazy('flags:flags'))
-#
-#
+class AddPicklistItem(TemplateView):
+    def get(self, request, **kwargs):
+        return form_page(request, add_picklist_item_form())
+
+    # def post(self, request, **kwargs):
+    #     response, status_code = post_flags(request, request.POST)
+    #     if status_code != 201:
+    #         return form_page(request, add_flag_form(), data=request.POST, errors=response.get('errors'))
+    #
+    #     return redirect(reverse_lazy('flags:flags'))
+
+
 # class EditFlag(TemplateView):
 #     def get(self, request, **kwargs):
 #         data, status_code = get_flag(request, str(kwargs['pk']))
@@ -61,37 +87,3 @@ class Picklist(TemplateView):
 #             'title': data['flag']['name']
 #         }
 #         return render(request, 'flags/profile.html', context)
-#
-#
-# class ChangeFlagStatus(TemplateView):
-#     def get(self, request, **kwargs):
-#         status = kwargs['status']
-#         description = ''
-#
-#         if status != 'deactivate' and status != 'reactivate':
-#             raise Http404
-#
-#         if status == 'deactivate':
-#             description = get_string('flags.update_flag.status.deactivate_warning')
-#
-#         if status == 'reactivate':
-#             description = get_string('flags.update_flag.status.reactivate_warning')
-#
-#         context = {
-#             'title': 'Are you sure you want to {} this flag?'.format(status),
-#             'description': description,
-#             'user_id': str(kwargs['pk']),
-#             'status': status,
-#         }
-#         return render(request, 'flags/change_status.html', context)
-#
-#     def post(self, request, **kwargs):
-#         status = kwargs['status']
-#
-#         if status != 'deactivate' and status != 'reactivate':
-#             raise Http404
-#
-#         put_flag(request, str(kwargs['pk']), json={'status': request.POST['status']})
-#
-#         return redirect(reverse_lazy('flags:flags'))
-#
