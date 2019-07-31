4972f8e2dc0924f7260eae55b47b9ba0d0f0f2d4
Jan Faracik <j.faracik@kainos.com>
2019-07-31 09:33:10 +0100

    fix

--- picklists/urls.py
+++ picklists/urls.py
@@ -6,10 +6,10 @@ app_name = 'picklists'
 urlpatterns = [
     # ex: /picklists/ - View all active picklists
     path('', views.Picklists.as_view(), name='picklists'),
-    # ex: /picklist/add/ -  add a new picklist item
+    # ex: /picklists/<uuid:pk>/ - View a picklist item
+    path('<uuid:pk>/', views.PicklistItem.as_view(), name='picklist_item'),
+    # ex: /picklist/add/ -  Add a new picklist item
     path('add/', views.AddPicklistItem.as_view(), name='add'),
-    # # ex: /picklists/<uuid:pk>/ - Go to a flags's detail
-    # path('<uuid:pk>/', views.PicklistItem.as_view(), name='picklist_item'),
     # # ex: /picklists/<uuid:pk>/edit/ - edit a picklist item
-    # path('<uuid:pk>/edit/', views.EditPicklistItem.as_view(), name='edit'),
+    path('<uuid:pk>/edit/', views.EditPicklistItem.as_view(), name='edit'),
 ]
