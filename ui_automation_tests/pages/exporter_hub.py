c5cbb21b0e1df1a1bc34c03062397fece2663346
Nick Philpott <nicholasp@kainos.com>
2019-07-31 11:05:21 +0100

    tests

--- ui_automation_tests/pages/exporter_hub.py
+++ ui_automation_tests/pages/exporter_hub.py
@@ -275,9 +275,9 @@ class ExporterHub():
 
     def select_is_your_good_intended_to_be_incorporated_into_an_end_product(self, option):
         if option == "Yes":
-            self.driver.find_element_by_id("is_good_end_product-yes").click()
-        else:
             self.driver.find_element_by_id("is_good_end_product-no").click()
+        else:
+            self.driver.find_element_by_id("is_good_end_product-yes").click()
 
     def enter_part_number(self, part_number):
         part_number_tb = self.driver.find_element_by_id("part_number")
