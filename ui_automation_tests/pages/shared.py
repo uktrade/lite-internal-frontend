35110be9b8168fabeb6b186b5540a552b975e13b
Jan Faracik <j.faracik@kainos.com>
2019-08-05 16:10:20 +0100

    Update shared.py

--- ui_automation_tests/pages/shared.py
+++ ui_automation_tests/pages/shared.py
@@ -1,9 +1,10 @@
-class Shared():
+from helpers.BasePage import BasePage
 
-    def __init__(self, driver):
-        self.driver = driver
-        self.submit_button = "[type*='submit']" #css
-        self.error_message = ".govuk-error-message" #css
+
+class Shared(BasePage):
+
+    submit_button = '[type*="submit"]'  # CSS
+    error_message = '.govuk-error-message'  # CSS
 
     def click_submit(self):
         self.driver.find_element_by_css_selector(self.submit_button).click()
@@ -12,13 +13,13 @@ class Shared():
         return self.driver.find_elements_by_css_selector(self.error_message)[no].text
 
     def get_text_of_body(self):
-        return self.driver.find_element_by_css_selector("body").text
+        return self.driver.find_element_by_css_selector('body').text
 
     def get_text_of_selected_tab(self):
-        return self.driver.find_element_by_css_selector(".lite-tabs__tab.selected").text
+        return self.driver.find_element_by_css_selector('.lite-tabs__tab .selected').text
 
     def get_text_of_table_body(self):
-        return self.driver.find_element_by_css_selector(".govuk-table__body").text
+        return self.driver.find_element_by_css_selector('.govuk-table__body').text
 
     def get_text_of_caption(self):
-        return self.driver.find_element_by_css_selector(".govuk-caption-l").text
+        return self.driver.find_element_by_css_selector('.govuk-caption-l').text
