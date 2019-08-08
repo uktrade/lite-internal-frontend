83bdae21d35b663287100e25cec698aa7ed4f7f2
Andy Taylor <a.taylor@kainos.com>
2019-08-08 15:12:09 +0100

    include param names

--- cases/forms/create_ecju_query.py
+++ cases/forms/create_ecju_query.py
@@ -1,11 +1,11 @@
 from core.builtins.custom_tags import get_string
-from libraries.forms.components import Form, BackLink, Question, InputType, HiddenField, Select
+from libraries.forms.components import Form, BackLink, Question, InputType, HiddenField, Select, Button, Group
 
 
 def create_ecju_queries_form(case, case_url, picklists):
-    return Form(get_string('cases.ecju_queries.title'),
-                '',
-                [
+    return Form(title=get_string('cases.ecju_queries.title'),
+                description='',
+                questions=[
                     Question(title=get_string('cases.ecju_queries.add_query.title'),
                              description=get_string('cases.ecju_queries.add_query.description'),
                              input_type=InputType.TEXTAREA,
@@ -14,8 +14,11 @@ def create_ecju_queries_form(case, case_url, picklists):
                                  'max_length': 5000,
                              }),
                     HiddenField(name='case', value=case),
-                    Select(title='Ask a quick question', description='Select a pre-filled question', name='picklist',
+                    Select(title='Ask a quick question', description='Select a pre-filled question',
+                           name='picklist',
                            options=picklists,
                            optional=True)
+
                 ],
-                back_link=BackLink('Back to ' + get_string('cases.ecju_queries.title'), case_url))
+                back_link=BackLink('Back to ' + get_string('cases.ecju_queries.title'), case_url)
+                )
