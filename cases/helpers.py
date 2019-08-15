a4fa510b8a0f68acf53b07575c94002bc98b0818
Jan Faracik <j.faracik@kainos.com>
2019-08-15 16:17:08 +0100

    banana

--- cases/helpers.py
+++ cases/helpers.py
@@ -9,11 +9,11 @@ def clean_advice(json):
     json['goods'] = _clean_dict_item(json['goods'])
     json['goods_types'] = _clean_dict_item(json['goods_types'])
     json['countries'] = _clean_dict_item(json['countries'])
-    json['end_user'] = _clean_dict_item(json['end_user'])
     json['ultimate_end_users'] = _clean_dict_item(json['ultimate_end_users'])
     json['denial_reasons'] = json.getlist('denial_reasons')
 
-    if json['end_user']:
+    if json.get('end_user') and isinstance(json.get('end_user'), list):
+        json['end_user'] = _clean_dict_item(json['end_user'])
         json['end_user'] = json['end_user'][0]
     else:
         json['end_user'] = None
