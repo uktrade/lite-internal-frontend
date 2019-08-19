fa57fc326bc4cb05e1f5b79c3f871dd80540672f
Jan Faracik <j.faracik@kainos.com>
2019-08-19 15:40:38 +0100

    Added session cookie domain

--- conf/settings.py
+++ conf/settings.py
@@ -175,7 +175,7 @@ SASS_PROCESSOR_ENABLED = True
 
 # Django Messages
 MESSAGE_STORAGE = 'django.contrib.messages.storage.fallback.FallbackStorage'
-SESSION_COOKIE_DOMAIN = 'uktrade.io'
+SESSION_COOKIE_DOMAIN = env('SESSION_COOKIE_DOMAIN')
 
 # File Upload
 # https://github.com/uktrade/s3chunkuploader
