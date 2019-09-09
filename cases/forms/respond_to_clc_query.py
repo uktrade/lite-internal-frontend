f77c2b0912d55c84a8be4d9d5eab7b2431db4449
Jan Faracik <j.faracik@kainos.com>
2019-09-09 10:26:17 +0100

    Clean up

--- cases/forms/respond_to_clc_query.py
+++ cases/forms/respond_to_clc_query.py
@@ -1,10 +1,23 @@
 from django.urls import reverse_lazy
-from lite_forms.components import Form, BackLink, RadioButtons, Option, TextInput, TextArea
+from lite_forms.components import Form, BackLink, RadioButtons, Option, TextInput, TextArea, HTMLBlock, Heading
+from lite_forms.styles import HeadingStyle
 
 
 def respond_to_clc_query_form(case):
     return Form(title='Respond to CLC Query',
                 questions=[
+                    Heading('CLC12-13279', HeadingStyle.S),
+                    HTMLBlock(html='<div class="app-summary-list app-inset-text">'
+                                        '<div class="app-summary-list__item">'
+                                            '<p class="govuk-caption-m">Description</p>'
+                                            '<p class="govuk-body-m">Lorem Ipsum</p>'
+                                        '</div>'
+                                        '<div class="app-summary-list__item">'
+                                            '<p class="govuk-caption-m">Control Code Classification</p>'
+                                            '<p class="govuk-body-m">ML1a</p>'
+                                        '</div>'
+                                    '</div>'),
+                    HTMLBlock(html='<hr class="lite-horizontal-separator">'),
                     RadioButtons(title='Is this good controlled?',
                                  description='Example description text',
                                  name='is_good_controlled',
