87805a6c18451037cb20dda4b67f785ab25f3c5c
cstobart <c.stobart@kainos.com>
2019-09-18 16:37:41 +0100

    Update finalise_case.py

--- cases/forms/finalise_case.py
+++ cases/forms/finalise_case.py
@@ -5,7 +5,6 @@ from lite_forms.components import Form, TextInput, BackLink, DateInput
 def approve_licence_form(case_id):
     return Form(
         title='Approve',
-        description='',
         questions=[
             DateInput(description='For example, 27 3 2007', title='When will the licence start?', prefix=''),
             TextInput(name='duration', description='This must be a whole number of months, such as 12', title='How long will it last?'),
@@ -17,6 +16,5 @@ def approve_licence_form(case_id):
 def refuse_licence_form(case_id):
     return Form(
         title='Refuse',
-        description='',
         back_link=BackLink(url=reverse_lazy('cases:final_advice_view', kwargs={'pk': case_id}), text='Back to final advice'),
     )
