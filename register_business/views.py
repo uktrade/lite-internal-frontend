9555a338a76720c31ed4a00dad85ded0cd8a6769
cstobart <c.stobart@kainos.com>
2019-09-25 10:46:49 +0100

    Update views.py

--- register_business/views.py
+++ register_business/views.py
@@ -1,81 +1,30 @@
 from django.contrib import messages
-from django.shortcuts import render, redirect
+from django.shortcuts import redirect
 from django.views.generic import TemplateView
-from lite_forms.components import HiddenField
 from lite_forms.generators import form_page
-from lite_forms.helpers import get_form_by_pk, get_next_form_after_pk, nest_data, flatten_data
+from lite_forms.submitters import submit_paged_form
 
 from organisations.services import post_organisations
-from register_business import forms
+from register_business.forms import register_business_forms
 
 
 class RegisterBusiness(TemplateView):
+    forms = None
+
+    def dispatch(self, request, *args, **kwargs):
+        individual = request.POST.get('sub_type') == 'individual'
+        self.forms = register_business_forms(individual)
+
+        return super(RegisterBusiness, self).dispatch(request, *args, **kwargs)
+
     def get(self, request, **kwargs):
-        return form_page(request, forms.register_business_forms().forms[0])
+        return form_page(request, self.forms.forms[0])
 
     def post(self, request, **kwargs):
-        data = request.POST.copy()
-        individual = data.get('sub_type') == 'individual'
-        register_business_forms = forms.register_business_forms(individual)
+        response, data = submit_paged_form(request, self.forms, post_organisations)
 
-        # Get the next form based off form_pk
-        current_form = get_form_by_pk(data.get('form_pk'), register_business_forms)
-        next_form = get_next_form_after_pk(data.get('form_pk'), register_business_forms)
+        if response:
+            return response
 
-        # Remove form_pk and CSRF from POST data as the new form will replace them
-        del data['form_pk']
-        del data['csrfmiddlewaretoken']
-
-        # Post the data to the validator and check for errors
-        nested_data = nest_data(data)
-        validated_data, _ = post_organisations(request, nested_data)
-
-        if 'errors' in validated_data:
-            for key, value in validated_data.get('errors').copy().items():
-                if value == ['This field is required.']:
-                    del validated_data.get('errors')[key]
-
-            # If there are errors in the validated data, take the user back
-            if len(validated_data['errors']) is not 0:
-
-                # TODO: Clean up this code
-                # Add hidden fields to the current form
-                for key, value in data.items():
-                    exists = False
-
-                    for question in current_form.questions:
-                        if hasattr(question, 'name'):
-                            if question.name == key:
-                                exists = True
-                                continue
-
-                    if not exists:
-                        current_form.questions.append(
-                            HiddenField(key, value)
-                        )
-
-                context = {
-                    'page': current_form,
-                    'title': current_form.title,
-                    'errors': flatten_data(validated_data['errors']),
-                    'data': data,
-                }
-                return render(request, 'form.html', context)
-
-        # If there aren't any forms left to go through, submit all the data and go to a success page
-        if next_form is None:
-            messages.success(request, 'The organisation was created successfully')
-            return redirect('organisations:organisations')
-
-        # Add existing post data to new form as hidden fields
-        for key, value in data.items():
-            next_form.questions.append(
-                HiddenField(key, value)
-            )
-
-        # Go to the next page
-        context = {
-            'page': next_form,
-            'title': next_form.title,
-        }
-        return render(request, 'form.html', context)
+        messages.success(request, 'The organisation was created successfully')
+        return redirect('organisations:organisations')
