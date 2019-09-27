f23444fa0bfc457ffac4c363e7d97f639d68585f
Abbas Ghulam <g.abbas@kainos.com>
2019-09-27 17:03:04 +0100

    Added support for a proxy while running e2e tests

--- ui_automation_tests/fixtures/core.py
+++ ui_automation_tests/fixtures/core.py
@@ -27,6 +27,10 @@ def driver(request):
         chrome_options.add_argument('--headless')
     chrome_options.add_argument('--no-sandbox')
 
+# Use proxy settings provided in config file for security testing
+    if os.environ.get('PROXY_IP_PORT') is not None:
+        chrome_options.add_argument('--proxy-server=%s' % str(os.environ.get('PROXY_IP_PORT')))
+
     if browser == 'chrome':
         if str(os.environ.get('ENVIRONMENT')) == 'None':
             browser = webdriver.Chrome("chromedriver", chrome_options=chrome_options)
