3be02d7d46af4284f69dd4583966f7b3049052fe
Jan Faracik <j.faracik@kainos.com>
2019-10-02 14:24:26 +0100

    Added initial form

--- letter_templates/forms.py
+++ letter_templates/forms.py
@@ -1,14 +1,41 @@
 from django.urls import reverse_lazy
-from lite_forms.components import Form, FormGroup, TextInput, BackLink
+from lite_forms.components import Form, FormGroup, TextInput, BackLink, Checkboxes, Option, RadioButtons
 
 
 def add_letter_template():
-    return FormGroup([
-        Form(title='Enter a name for your letter template',
-             description='This will make it easier to find your template in the future',
-             questions=[
-                 TextInput(name='name')
-             ],
-             back_link=BackLink('Back to letter templates', reverse_lazy('letter_templates:letter_templates')),
-             default_button_name='Continue')
-    ], show_progress_indicators=True)
+    return FormGroup(
+        forms=[
+            Form(title='Enter a name for your letter template',
+                 description='This will make it easier to find in the future.',
+                 questions=[
+                     TextInput(name='name')
+                 ],
+                 back_link=BackLink('Back to letter templates', reverse_lazy('letter_templates:letter_templates')),
+                 default_button_name='Continue'),
+            Form(title='What types of case can this template apply to?',
+                 description='Select all case types that apply.',
+                 questions=[
+                     Checkboxes(
+                         name='restricted_to',
+                         options=[
+                             Option('standard_licence_applications', 'Standard Licence Applications'),
+                             Option('open_licence_applications', 'Open Licence Applications'),
+                             Option('control_list_classification_queries', 'Control List Classification Queries'),
+                             Option('end_user_advisory_queries', 'End User Advisory Queries'),
+                         ])
+                 ],
+                 default_button_name='Continue'),
+            Form(title='Select a layout to use for this letter template',
+                 questions=[
+                     RadioButtons(
+                         name='restricted_to',
+                         options=[
+                             Option('standard_licence_applications', 'Standard Licence Applications'),
+                             Option('open_licence_applications', 'Open Licence Applications'),
+                             Option('control_list_classification_queries', 'Control List Classification Queries'),
+                             Option('end_user_advisory_queries', 'End User Advisory Queries'),
+                         ])
+                 ],
+                 default_button_name='Continue')
+        ],
+        show_progress_indicators=True)
--- letter_templates/views.py
+++ letter_templates/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 from lite_forms.generators import form_page
+from lite_forms.submitters import submit_paged_form
 
 from letter_templates.forms import add_letter_template
 
@@ -14,7 +15,18 @@ class LetterTemplates(TemplateView):
         return render(request, 'letter_templates/letter_templates.html', context)
 
 
+# Remove this!
+def return_success(test, test2):
+    return {'success': True}, True
+
+
 class Add(TemplateView):
 
     def get(self, request, **kwargs):
         return form_page(request, add_letter_template().forms[0])
+
+    def post(self, request, **kwargs):
+        response, _ = submit_paged_form(request, add_letter_template(), return_success)
+
+        if response:
+            return response
