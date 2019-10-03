9aabb07dc2904a2f50ce930202c2de9ddb2d96ad
cstobart <c.stobart@kainos.com>
2019-10-03 14:13:37 +0100

    Update urls.py

--- organisations/urls.py
+++ organisations/urls.py
@@ -5,10 +5,10 @@ from organisations import views
 app_name = 'organisations'
 
 urlpatterns = [
-    # ex: /
-    path('organisations/', views.OrganisationList.as_view(), name='organisations'),
-    # ex: /43a88949-5db9-4334-b0cc-044e91827451/
-    path('organisations/<uuid:pk>/', views.OrganisationDetail.as_view(), name='organisation'),
-    # ex: /hmrc/
-    path('organisations/hmrc/', views.HMRCList.as_view(), name='hmrc')
+    # ex: /organisations/
+    path('', views.OrganisationList.as_view(), name='organisations'),
+    # ex: /organisations/43a88949-5db9-4334-b0cc-044e91827451/
+    path('<uuid:pk>/', views.OrganisationDetail.as_view(), name='organisation'),
+    # ex: /organisations/hmrc/
+    path('hmrc/', views.HMRCList.as_view(), name='hmrc')
 ]
