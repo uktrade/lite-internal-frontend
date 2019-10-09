9b5b8d0fd797a1d457436a902ca0e2152e1ba06f
cstobart <c.stobart@kainos.com>
2019-10-09 10:05:06 +0100

    ...

--- cases/services.py
+++ cases/services.py
@@ -292,16 +292,16 @@ def _generate_post_data_and_errors(keys, request_data, action):
 
 def _get_all_distinct_flags(case):
     flags = []
-    flags.extend(case['flags'])
-    flags.extend(case['application']['organisation']['flags'])
-    if 'goods_types' in case['application']:
-        for good in case['application']['goods_types']:
-            for flag in good['flags']:
+    flags.extend(case.get('flags'))
+    flags.extend(case.get('application').get('organisation').get('flags'))
+    if 'goods_types' in case.get('application'):
+        for good in case.get('application').get('goods_types'):
+            for flag in good.get('flags'):
                 if flag not in flags:
                     flags.append(flag)
-    elif 'goods' in case['application']:
-        for good in case['application']['goods']:
-            for flag in good['good']['flags']:
+    elif 'goods' in case.get('application'):
+        for good in case.get('application').get('goods'):
+            for flag in good.get('good').get('flags'):
                 if flag not in flags:
                     flags.append(flag)
     return flags
