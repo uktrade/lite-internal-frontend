cac588a7ae59d79abe76c7d3b491c68d2ef781a7
cstobart <c.stobart@kainos.com>
2019-10-10 11:11:44 +0100

    Update organisation_page.py

--- ui_automation_tests/pages/organisation_page.py
+++ ui_automation_tests/pages/organisation_page.py
@@ -1,6 +1,3 @@
-from helpers.helpers import highlight
-
-
 class OrganisationPage:
     def __init__(self, driver):
         self.driver = driver
@@ -14,7 +11,6 @@ class OrganisationPage:
     def is_organisation_flag_applied(self, flag_name):
         elements = self.driver.find_elements_by_css_selector(self.flags_area)
         for element in elements:
-            highlight(element)
             if flag_name in element.text:
                 return True
         return False
