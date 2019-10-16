1e408d4e74d1b18a659833705298aaec79b8dbcd
Craig Anderson <craig.anderson@digital.trade.gov.uk>
2019-10-16 14:41:00 +0100

    Sort letter paragraphs in the same way as the list of IDs.

---
+++ letter_templates/tests.py
@@ -0,0 +1,18 @@
+from unittest import TestCase
+
+from letter_templates.views.manage import LetterTemplateEditLetterParagraphs
+
+
+class LetterTemplateEditLetterParagraphsTestCase(TestCase):
+    def test_sort_letter_paragraphs(self):
+        ids = ["abc", "def", "uvw", "xyz"]
+        result = LetterTemplateEditLetterParagraphs.sort_letter_paragraphs([
+                {"id": "xyz", "label": "last"},
+                {"id": "abc", "label": "first"},
+                {"id": "uvw", "label": "third"},
+                {"id": "def", "label": "second"},
+            ],
+            ids,
+        )
+        result_ids = [r["id"] for r in result]
+        self.assertEqual(ids, result_ids)
