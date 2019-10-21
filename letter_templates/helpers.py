619b6bc80c931ef092b6f9e6d3fb7d6db4338091
Jan Faracik <j.faracik@kainos.com>
2019-10-21 16:08:17 +0100

    fix

--- letter_templates/helpers.py
+++ letter_templates/helpers.py
@@ -12,5 +12,20 @@ def generate_preview(layout, letter_paragraphs: list):
 
     letter_context = {
         'content': '\n\n'.join([x['text'] for x in letter_paragraphs]),
+        'applicant': {
+            'name': '{{ applicant.name }}',
+            'primary_site': {
+                'address': {
+                    'address_line_1': '{{ applicant.primary_site.address.address_line_1 }}',
+                    'address_line_2': '{{ applicant.primary_site.address.address_line_2 }}',
+                    'postcode': '{{ applicant.primary_site.address.postcode }}',
+                    'city': '{{ applicant.primary_site.address.city }}',
+                    'region': '{{ applicant.primary_site.address.region }}',
+                    'country': {
+                        'name': '{{ applicant.primary_site.address.country.name }}'
+                    }
+                }
+            }
+        }
     }
     return template.render(letter_context)
