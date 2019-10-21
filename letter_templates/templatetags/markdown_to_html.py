af4e245648d4c20704022c23fcaf060c6e3427f7
Jan Faracik <j.faracik@kainos.com>
2019-10-21 13:02:21 +0100

    Update markdown_to_html.py

--- letter_templates/templatetags/markdown_to_html.py
+++ letter_templates/templatetags/markdown_to_html.py
@@ -1,8 +1,8 @@
+import bleach
 from django import template
 from django.template import engines
 
 from markdown import Markdown
-
 
 register = template.Library()
 
@@ -13,10 +13,12 @@ def markdown_to_html(context):
     Template tag to render the html and replace
     placeholder tags with user requested params
     """
+    # Clean initial content
+    text = bleach.clean(context.get('content', ''))
 
     # Convert markdown text to html
     md = Markdown()
-    text = md.convert(context.get('content', ''))
+    text = md.convert(text)
     if not text:
         return ''
 
