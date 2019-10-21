4eaa06046c0865371668a58e3d9c7318e26a886f
Jayesh Patel <ejayesh@hotmail.com>
2019-10-21 17:28:50 +0100

    Var to use ZAP

--- ui_automation_tests/docker-entry.sh
+++ ui_automation_tests/docker-entry.sh
@@ -2,10 +2,13 @@
 
 set -x
 
-# Start zap proxy deamon
-/usr/share/owasp-zap/zap.sh -daemon -config api.addrs.addr.regex=true -config api.disablekey=true -port 8090 -host 127.0.0.1 &
-# Wait 10 seconds for zap daemon to start, sleep 10 will not work, the sleep will run in background.
-for x in {1..10}; do sleep 1; done
+if [[ "$RUN_ZAP" = true ]]; then
+  export PROXY_IP_PORT="127.0.0.1:8090"
+  # Start zap proxy deamon
+  /usr/share/owasp-zap/zap.sh -daemon -config api.addrs.addr.regex=true -config api.disablekey=true -port 8090 -host 127.0.0.1 &
+  # Wait 10 seconds for zap daemon to start, sleep 10 will not work, the sleep will run in background.
+  for x in {1..10}; do sleep 1; done
+fi
 
 # run py.test ($@ to derive parameters from commandline)
 py.test -k "$TESTS_TO_RUN" --alluredir=ui_automation_tests/allure-results &
@@ -16,5 +19,7 @@ trap "echo '=== Stopping PID $pid ==='; kill -SIGTERM $pid" SIGINT SIGTERM
 
 wait $pid
 
-# generate zap proxy report
-bash ui_automation_tests/zap/run.sh
+if [[ "$RUN_ZAP" = true ]]; then
+  # generate zap proxy report
+  bash ui_automation_tests/zap/run.sh
+fi
