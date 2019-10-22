865b02ce34de4d3a3f8dd6985f6abc70cf7be716
BenAndrew-Kainos <bena@kainos.com>
2019-10-22 10:08:03 +0100

    Refactor template preview into seperate endpoint

--- letter_templates/views/generator.py
+++ letter_templates/views/generator.py
@@ -10,20 +10,6 @@ from letter_templates.services import get_letter_paragraphs, post_letter_templat
 from picklists.services import get_picklists
 
 
-def generate_preview(request, letter_paragraphs, name, layout, restricted_to):
-    letter_paragraphs = get_letter_paragraphs(request, letter_paragraphs)
-
-    preview = helpers.generate_preview(layout, letter_paragraphs)
-
-    return render(request, 'letter_templates/preview.html', {
-        'preview': preview,
-        'name': name,
-        'layout': layout,
-        'restricted_to': restricted_to,
-        'letter_paragraphs': letter_paragraphs
-    })
-
-
 def generate_generator(request, letter_paragraphs, name, layout, restricted_to):
     letter_paragraphs = get_letter_paragraphs(request, letter_paragraphs)
     return render(request, 'letter_templates/generator.html', {'letter_paragraphs': letter_paragraphs,
@@ -50,47 +36,59 @@ class Add(TemplateView):
                                   restricted_to=request.POST.getlist('restricted_to'))
 
 
+def get_template_content(request):
+    data = request.POST.copy()
+    return {
+        "name": data.get('name'),
+        "layout": data.get('layout'),
+        "restricted_to": data.getlist('restricted_to'),
+        "action": data.get('action'),
+        "letter_paragraphs": data.getlist('letter_paragraphs')
+    }
+
+
 class LetterParagraphs(TemplateView):
-
-    def dispatch(self, request, *args, **kwargs):
-        data = request.POST.copy()
-        name = data.get('name')
-        layout = data.get('layout')
-        restricted_to = data.getlist('restricted_to')
-        action = data.get('action')
-        existing_letter_paragraphs = data.getlist('letter_paragraphs')
-
-        if action == 'add_letter_paragraph':
+    def post(self, request):
+        template_content = get_template_content(request)
+        if template_content['action'] == 'add_letter_paragraph':
             all_letter_paragraphs = get_picklists(request, 'letter_paragraph')
             context = {
-                'name': name,
-                'layout': layout,
-                'restricted_to': restricted_to,
+                'name': template_content['name'],
+                'layout': template_content['layout'],
+                'restricted_to': template_content['restricted_to'],
                 'letter_paragraphs': [x for x in all_letter_paragraphs['picklist_items'] if
-                                      x['id'] not in existing_letter_paragraphs],
-                'existing_letter_paragraphs': existing_letter_paragraphs
+                                      x['id'] not in template_content['letter_paragraphs']],
+                'existing_letter_paragraphs': template_content['letter_paragraphs']
             }
             return render(request, 'letter_templates/letter_paragraphs.html', context)
-        elif action == 'preview':
-            return generate_preview(request,
-                                    existing_letter_paragraphs,
-                                    name=name,
-                                    layout=layout,
-                                    restricted_to=restricted_to)
-        elif 'delete' in action:
-            pk_to_delete = action.split('.')[1]
-            existing_letter_paragraphs.remove(pk_to_delete)
+        elif 'delete' in template_content['action']:
+            pk_to_delete = template_content['action'].split('.')[1]
+            template_content['letter_paragraphs'].remove(pk_to_delete)
 
         return generate_generator(request,
-                                  letter_paragraphs=existing_letter_paragraphs,
-                                  name=name,
-                                  layout=layout,
-                                  restricted_to=restricted_to)
+                                  letter_paragraphs=template_content['letter_paragraphs'],
+                                  name=template_content['name'],
+                                  layout=template_content['layout'],
+                                  restricted_to=template_content['restricted_to'])
 
 
 class Preview(TemplateView):
-    def post(self, request, **kwargs):
-        post_letter_templates(request, request.POST)
+    def post(self, request):
+        template_content = get_template_content(request)
+        letter_paragraphs = get_letter_paragraphs(request, template_content['letter_paragraphs'])
+        preview = helpers.generate_preview(template_content['layout'], letter_paragraphs)
 
+        return render(request, 'letter_templates/preview.html', {
+            'preview': preview,
+            'name': template_content['name'],
+            'layout': template_content['layout'],
+            'restricted_to': template_content['restricted_to'],
+            'letter_paragraphs': template_content['letter_paragraphs']
+        })
+
+
+class Create(TemplateView):
+    def post(self, request):
+        post_letter_templates(request, request.POST)
         messages.success(request, 'The letter template was created successfully')
         return redirect('letter_templates:letter_templates')
