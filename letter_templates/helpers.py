55c48517a441beacf362c1cfa4d36d6415c3e058
BenAndrew-Kainos <bena@kainos.com>
2019-10-22 16:28:26 +0100

    Fix template preview context variables

--- letter_templates/helpers.py
+++ letter_templates/helpers.py
@@ -1,35 +1,20 @@
 import os
 
 from django.shortcuts import render
-from django.template import engines
+from django.template import Engine, Context
 
 from conf import settings
 from letter_templates.services import get_letter_paragraphs
 
 
 def generate_preview(layout, letter_paragraphs: list):
-    django_engine = engines['django']
-    template = django_engine.from_string(
-        open(os.path.join(settings.LETTER_TEMPLATES_DIRECTORY, f'{layout}.html'), 'r').read())
+    django_engine = Engine(string_if_invalid='{{ %s }}')
+    with open(os.path.join(settings.LETTER_TEMPLATES_DIRECTORY, f'{layout}.html'), 'r') as html_file:
+        template = django_engine.from_string(html_file.read())
 
-    letter_context = {
-        'content': '<br><br>'.join([x['text'] for x in letter_paragraphs]),
-        'applicant': {
-            'name': '{{ applicant.name }}',
-            'primary_site': {
-                'address': {
-                    'address_line_1': '{{ applicant.primary_site.address.address_line_1 }}',
-                    'address_line_2': '{{ applicant.primary_site.address.address_line_2 }}',
-                    'postcode': '{{ applicant.primary_site.address.postcode }}',
-                    'city': '{{ applicant.primary_site.address.city }}',
-                    'region': '{{ applicant.primary_site.address.region }}',
-                    'country': {
-                        'name': '{{ applicant.primary_site.address.country.name }}'
-                    }
-                }
-            }
-        }
-    }
+    letter_context = Context({
+        'content': '<br><br>'.join([x['text'] for x in letter_paragraphs])
+    })
     return template.render(letter_context)
 
 
