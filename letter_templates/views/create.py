570c8b20bd17663c7aa610bf25eb8e414eeadb1a
Craig Anderson <craig.anderson@digital.trade.gov.uk>
2019-10-28 16:24:18 +0000

    Fix issues found by prospector.

--- letter_templates/views/create.py
+++ letter_templates/views/create.py
@@ -1,14 +1,13 @@
 from django.contrib import messages
-from django.shortcuts import render, redirect
+from django.shortcuts import redirect
 from django.views.generic import TemplateView
 from lite_forms.generators import form_page
 from lite_forms.submitters import submit_paged_form
 
 from core.builtins.custom_tags import get_string
-from letter_templates import helpers
 from letter_templates.forms import add_letter_template
 from letter_templates.helpers import get_template_content
-from letter_templates.services import get_letter_paragraphs, post_letter_template
+from letter_templates.services import post_letter_template
 from letter_templates.views.letter_paragraphs import get_order_paragraphs_page
 
 
