05c2462fa41b6391791c277ef24fc1b065ed9ba7
BenAndrew-Kainos <bena@kainos.com>
2019-10-31 13:25:19 +0000

    Change back to error page

--- letter_templates/views/create.py
+++ letter_templates/views/create.py
@@ -1,7 +1,7 @@
 from django.contrib import messages
 from django.shortcuts import redirect
 from django.views.generic import TemplateView
-from lite_forms.generators import form_page
+from lite_forms.generators import form_page, error_page
 from lite_forms.submitters import submit_paged_form
 
 from core.builtins.custom_tags import get_string
@@ -35,9 +35,14 @@ class Create(TemplateView):
         json = request.POST.copy()
         json['letter_paragraphs'] = request.POST.getlist('letter_paragraphs')
         json['restricted_to'] = request.POST.getlist('restricted_to')
-        data, status_code = post_letter_template(request, json)
+        response, status_code = post_letter_template(request, json)
         if status_code == 201:
             messages.success(request, get_string('letter_templates.letter_templates.successfully_created_banner'))
         else:
-            messages.error(request, get_string('letter_templates.letter_templates.error_banner'))
+            error_messages = []
+            errors = response["errors"]
+            for field, field_errors in errors.items():
+                for field_error in field_errors:
+                    error_messages.append(field_error)
+            return error_page(None, "; ".join(error_messages))
         return redirect('letter_templates:letter_templates')
