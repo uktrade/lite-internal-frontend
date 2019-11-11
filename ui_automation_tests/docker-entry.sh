9247e02c25b53f38ed0a32aa2605e96dcbf943f6
Nick Philpott <nicholasp@kainos.com>
2019-11-11 10:11:54 +0000

    parallel param

--- ui_automation_tests/docker-entry.sh
+++ ui_automation_tests/docker-entry.sh
@@ -2,7 +2,7 @@
 
 set -x
 # run py.test ($@ to derive parameters from commandline)
-py.test -k "$TESTS_TO_RUN"  -n=3 --dist=loadscope --alluredir=ui_automation_tests/allure-results &
+py.test -k "$TESTS_TO_RUN"  -n=$NO_OF_PARALLEL_RUNNERS --dist=loadscope --alluredir=ui_automation_tests/allure-results &
 pid="$!"
 
 # trap process id to stop script using Control+C
