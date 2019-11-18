cb70bc93bff9307fc9c17c77c3a7ccd3ce73d5a0
BenAndrew-Kainos <bena@kainos.com>
2019-11-18 13:56:35 +0000

    Added html to pdf processing

--- cases/views/generate_document.py
+++ cases/views/generate_document.py
@@ -1,12 +1,19 @@
+import uuid
+
 from django.shortcuts import redirect, render
 from django.urls import reverse_lazy
 from django.views.generic import TemplateView
+from weasyprint import HTML, CSS
 
 from cases.forms.generate_document import select_template_form
 from cases.services import get_case
+from conf.settings import BASE_DIR
 from letter_templates.helpers import generate_preview, paragraphs_to_markdown
 from letter_templates.services import get_letter_templates, get_letter_template, get_letter_paragraphs
 from lite_forms.generators import form_page
+
+
+CSS_LOCATION = '/assets/css/styles.css'
 
 
 class SelectTemplate(TemplateView):
@@ -24,15 +31,31 @@ class SelectTemplate(TemplateView):
 
 
 class PreviewDocument(TemplateView):
-    def get(self, request, **kwargs):
-        template_id = str(kwargs["tpk"])
-        case_id = str(kwargs["pk"])
-        template = get_letter_template(request, template_id)
-        case = get_case(request, case_id)
+    template_id: str
+    case_id: str
+    template: object
+    case: object
+    content: dict
+    preview: str
 
-        content = {
-            "content": paragraphs_to_markdown(get_letter_paragraphs(request, template["letter_paragraphs"])),
-            "case": case,
+    def dispatch(self, request, *args, **kwargs):
+        self.template_id = str(kwargs["tpk"])
+        self.case_id = str(kwargs["pk"])
+        self.template = get_letter_template(request, self.template_id)
+        self.case = get_case(request, self.case_id)
+        self.content = {
+            "content": paragraphs_to_markdown(get_letter_paragraphs(request, self.template["letter_paragraphs"])),
+            "case": self.case,
         }
-        preview = generate_preview(template["layout"]["filename"], content)
-        return render(request, "letter_templates/preview.html", {"preview": preview})
+        self.preview = generate_preview(self.template["layout"]["filename"], self.content)
+        return super(PreviewDocument, self).dispatch(request, *args, **kwargs)
+
+    def get(self, request, **kwargs):
+        return render(request, "generated_documents/preview.html",
+                      {"preview": self.preview, "pk": self.case_id, "tpk": self.template_id})
+
+    def post(self, request, **kwargs):
+        html = HTML(string=self.preview)
+        css = CSS(filename=BASE_DIR+CSS_LOCATION)
+        html.write_pdf(f'/tmp/{uuid.uuid4()}.pdf', stylesheets=[css])
+        return redirect(reverse_lazy("cases:case", kwargs={"pk": self.case_id}))
