41596529b3e8bbd2358eec2cafa2e796916723fd
Jan Faracik <j.faracik@kainos.com>
2019-11-20 16:09:11 +0000

    fix tests

--- cases/views/main.py
+++ cases/views/main.py
@@ -164,9 +164,9 @@ class ManageCase(TemplateView):
             ]
         }
 
-        if case["type"]["key"] == APPLICATION:
+        if case["type"]["key"] == case_types.APPLICATION:
             title = "Manage " + case.get("application").get("name")
-        elif case["type"]["key"] == HMRC_QUERY:
+        elif case["type"]["key"] == case_types.HMRC_QUERY:
             title = "Manage HMRC query"
         elif case["query"]["end_user"]:
             title = "Manage End User Advisory"
@@ -180,10 +180,10 @@ class ManageCase(TemplateView):
         case_id = str(kwargs["pk"])
         case = get_case(request, case_id)
 
-        if case["type"]["key"] == APPLICATION or case["type"]["key"] == HMRC_QUERY:
+        if case["type"]["key"] == case_types.APPLICATION or case["type"]["key"] == case_types.HMRC_QUERY:
             application_id = case.get("application").get("id")
             put_application_status(request, application_id, request.POST)
-        elif case["type"]["key"] == END_USER_ADVISORY_QUERY:
+        elif case["type"]["key"] == case_types.END_USER_ADVISORY_QUERY:
             query_id = case.get("query").get("id")
             put_end_user_advisory_query(request, query_id, request.POST)
         else:
