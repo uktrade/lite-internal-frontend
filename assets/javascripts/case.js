afa0cfa8f8c3461ab25d8fe9ffd42069b2326366
Jan Faracik <j.faracik@kainos.com>
2019-11-21 11:08:30 +0000

    Cleanup

--- assets/javascripts/case.js
+++ assets/javascripts/case.js
@@ -1,3 +1,4 @@
+// This may get added back in the future
 // $( '#link-organisation' ).click(function(e) {
 // 	e.preventDefault();
 // 	LITECommon.Modal.showModal('', $( '#modal-organisation' ).html(), false);
