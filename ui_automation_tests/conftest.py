ef58f63cae4d893f7cb962057963eb2fc3902beb
Nick Philpott <16895840+phlptt@users.noreply.github.com>
2019-11-22 13:19:53 +0000

    test: New test for licence templates

--- ui_automation_tests/conftest.py
+++ ui_automation_tests/conftest.py
@@ -14,7 +14,11 @@ from ui_automation_tests.fixtures.add_a_flag import (  # noqa
 )
 from ui_automation_tests.fixtures.add_queue import add_queue  # noqa
 from ui_automation_tests.fixtures.add_a_team import add_a_team  # noqa
-from ui_automation_tests.fixtures.add_a_document_template import add_a_document_template, get_template_id  # noqa
+from ui_automation_tests.fixtures.add_a_document_template import (  # noqa
+    add_a_document_template,
+    get_template_id,
+    get_licence_template_id,
+)
 from ui_automation_tests.fixtures.add_a_picklist import (  # noqa
     add_a_letter_paragraph_picklist,
     add_an_ecju_query_picklist,
