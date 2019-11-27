f6cbb0af479826017952c0c959bb52acd8da6821
BenAndrew-Kainos <bena@kainos.com>
2019-11-27 12:20:20 +0000

    Content changes

--- ui_automation_tests/pages/generate_document_page.py
+++ ui_automation_tests/pages/generate_document_page.py
@@ -4,7 +4,7 @@ from ui_automation_tests.shared.BasePage import BasePage
 class GeneratedDocument(BasePage):
     PREVIEW = ".app-letter-preview__page"  # css
     PARAGRAPHS = "paragraph_content"  # ID
-    DOWNLOAD = "download"  # ID
+    DOWNLOAD = "link-download"  # ID
 
     def click_letter_template(self, document_template_name):
         self.driver.find_element_by_id(document_template_name).click()
