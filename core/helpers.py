401a1b6e5c880c7e5ae3cccd21a71df5018e03b6
Jan Faracik <43062514+jfaracik@users.noreply.github.com>
2019-12-06 17:14:30 +0000

    feat: LT-1671 - Permissions to create and edit document templates (#249)

--- core/helpers.py
+++ core/helpers.py
@@ -1,3 +1,8 @@
+from conf import decorators
+from conf.constants import Permission
+from core.services import get_user_permissions
+
+
 def convert_dict_to_query_params(dictionary):
     items = []
     for key, value in dictionary.items():
@@ -6,4 +11,29 @@ def convert_dict_to_query_params(dictionary):
                 items.append(key + "=" + str(val))
         else:
             items.append(key + "=" + str(value))
-    return "&".join((items))
+    return "&".join(items)
+
+
+def has_permission(request, permission: Permission):
+    """
+    Returns true if the user has a given permission, else false
+    """
+    user_permissions = get_user_permissions(request)
+    return permission.value in user_permissions
+
+
+def decorate_patterns_with_permission(patterns, permission: Permission):
+    def _wrap_with_permission(_permission: Permission, view_func=None):
+        actual_decorator = decorators.has_permission(_permission)
+
+        if view_func:
+            return actual_decorator(view_func)
+        return actual_decorator
+
+    decorated_patterns = []
+    for pattern in patterns:
+        callback = pattern.callback
+        pattern.callback = _wrap_with_permission(permission, callback)
+        pattern._callback = _wrap_with_permission(permission, callback)
+        decorated_patterns.append(pattern)
+    return decorated_patterns
