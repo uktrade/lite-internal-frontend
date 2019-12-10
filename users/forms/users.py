e2a0d430308b9890cdb4a78174dd5213e9d374a3
Christopher Stobart <43062405+cstobart@users.noreply.github.com>
2019-12-10 10:10:37 +0000

    feat: LT-1622 - Restrict Roles and Permissions Visible to User (#251)

--- users/forms/users.py
+++ users/forms/users.py
@@ -2,8 +2,6 @@ from django.urls import reverse_lazy
 
 from lite_content.lite_internal_frontend import strings
 from lite_forms.components import Form, Select, TextInput, BackLink
-
-from core.builtins.custom_tags import get_string
 from lite_forms.helpers import conditional
 from teams.services import get_teams
 from users.services import get_roles
@@ -11,26 +9,26 @@ from users.services import get_roles
 
 def add_user_form(request):
     return Form(
-        title=get_string("users.invite"),
+        title=strings.USER_ADD_TITLE,
         questions=[
-            TextInput(title="What's the user's email?", name="email"),
-            Select(name="team", title="What team will the user belong to?", options=get_teams(request, True)),
-            Select(name="role", options=get_roles(request, True), title="What role should this user have?"),
+            TextInput(title=strings.USER_EMAIL_QUESTION, name="email"),
+            Select(name="team", title=strings.USER_TEAM_QUESTION, options=get_teams(request, True)),
+            Select(name="role", options=get_roles(request, True), title=strings.USER_ROLE_QUESTION),
         ],
-        back_link=BackLink("Back to Users", reverse_lazy("users:users")),
+        back_link=BackLink(strings.USER_ADD_FORM_BACK_TO_USERS, reverse_lazy("users:users")),
     )
 
 
-def edit_user_form(request, user_id, super_user):
+def edit_user_form(request, user_id, can_edit_role: bool):
     return Form(
         title="Edit User",
         questions=[
-            TextInput(title="Email", name="email"),
-            Select(name="team", title="What team will the user belong to?", options=get_teams(request, True)),
+            TextInput(title=strings.USER_EMAIL_QUESTION, name="email"),
+            Select(name="team", title=strings.USER_TEAM_QUESTION, options=get_teams(request, True)),
             conditional(
-                not super_user, Select(name="role", options=get_roles(request, True), title=strings.USER_ROLE_QUESTION),
+                can_edit_role, Select(name="role", options=get_roles(request, True), title=strings.USER_ROLE_QUESTION),
             ),
         ],
-        back_link=BackLink("Back to User", reverse_lazy("users:user", kwargs={"pk": user_id})),
-        default_button_name="Save",
+        back_link=BackLink(strings.USER_EDIT_FORM_BACK_TO_USER, reverse_lazy("users:user", kwargs={"pk": user_id})),
+        default_button_name=strings.USER_EDIT_FORM_SAVE,
     )
