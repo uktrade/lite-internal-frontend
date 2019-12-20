6e1e700367a203b2374de7a91b86ecf0c1f4abca
Nick Philpott <nicholasp@kainos.com>
2019-12-20 10:19:27 +0000

    aaa

--- ui_automation_tests/pages/assign_flags_to_case.py
+++ ui_automation_tests/pages/assign_flags_to_case.py
@@ -2,6 +2,6 @@ from shared.BasePage import BasePage
 
 
 class CaseFlagsPages(BasePage):
-    def select_flag(self, flag_id):
-        flag = self.driver.find_elements_by_id(flag_id)[0]
+    def select_flag(self, flag_name):
+        flag = self.driver.find_elements_by_id(flag_name)[0]
         flag.click()
