1806c6d7a74f594933921a7ee9d2a9a30f152854
Mark Spiers <marksp@kainos.com>
2020-01-03 10:50:42 +0000

    updated based on code-review

--- ui_automation_tests/pages/case_officer_pages.py
+++ ui_automation_tests/pages/case_officer_pages.py
@@ -1,32 +1,33 @@
 from shared.BasePage import BasePage
 
+from ui_automation_tests.shared import functions
+
 
 class CaseOfficerPages(BasePage):
-    ASSIGN_BTN = "assign"  # id
-    UNASSIGN_BTN = "unassign"  # id
+    BUTTON_ASSIGN_ID = "button-assign"  # id
+    BUTTON_UNASSIGN_ID = "button-unassign"  # id
     CHOICES = ".govuk-radios__item"  # css
     CHOICES_NAMES = ".govuk-radios__item label"
     CURRENT_CASE_OFFICER_PANEL = ".app-case-board"
-    SEARCH_TXT = "search"  # id
-    SEARCH_BTN = ".lite-search__button"  # css
+    TEXTAREA_SEARCH_ID = "textarea-search"  # id
+    BUTTON_SEARCH_ID = "button-search"  # id
     RADIO_BUTTON = "input[name='user']"
 
     def select_first_user(self):
         self.driver.find_elements_by_css_selector(self.RADIO_BUTTON)[0].click()
 
     def click_assign(self):
-        self.driver.find_element_by_id(self.ASSIGN_BTN).click()
+        self.driver.find_element_by_id(self.BUTTON_ASSIGN_ID).click()
 
     def click_unassign(self):
-        self.driver.find_element_by_id(self.UNASSIGN_BTN).click()
+        self.driver.find_element_by_id(self.BUTTON_UNASSIGN_ID).click()
 
     def search(self, text):
-        self.driver.find_element_by_id(self.SEARCH_TXT).send_keys(text)
-        self.driver.find_element_by_css_selector(self.SEARCH_BTN).click()
+        self.driver.find_element_by_id(self.TEXTAREA_SEARCH_ID).send_keys(text)
+        self.driver.find_element_by_id(self.BUTTON_SEARCH_ID).click()
 
     def get_users_name(self):
         return self.driver.find_elements_by_css_selector(self.CHOICES_NAMES)
 
     def is_current_case_officer(self):
-        # returns 0 or 1 depending on if element exists
-        return len(self.driver.find_elements_by_css_selector(self.CURRENT_CASE_OFFICER_PANEL))
+        return functions.element_with_css_selector_exists(self.driver, self.CURRENT_CASE_OFFICER_PANEL)
