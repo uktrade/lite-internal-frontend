0a788d36b84f28f24bd08c39b4e68c8a970c3382
Nick Philpott <nicholasp@kainos.com>
2020-01-06 15:49:46 +0000

    decouple the .env

--- ui_automation_tests/fixtures/env.py
+++ ui_automation_tests/fixtures/env.py
@@ -1,6 +1,14 @@
+import os
+
+from environ import Env
 from pytest import fixture
 
-from conf.settings import env
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+ENV_FILE = os.path.join(BASE_DIR, ".env")
+if os.path.exists(ENV_FILE):
+    Env.read_env(ENV_FILE)
+
+env = Env()
 
 
 @fixture(scope="session")
