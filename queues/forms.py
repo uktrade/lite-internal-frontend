60dd53f556af82e0b3f5830b27de2d8937dda5f2
Jan Faracik <j.faracik@kainos.com>
2020-01-16 17:01:01 +0000

    consistent strings

--- queues/forms.py
+++ queues/forms.py
@@ -6,15 +6,21 @@ from lite_forms.components import Form, TextInput, BackLink
 
 def new_queue_form():
     return Form(
-        title=strings.Queues.QueueAdd.PAGE_HEADING,
-        questions=[TextInput(title=strings.Queues.QueueAdd.QUESTION_TITLE, name="name")],
-        back_link=BackLink("Back to queues", reverse_lazy("queues:queues")),
+        title=strings.Queues.QueueAdd.TITLE,
+        description=strings.Queues.QueueAdd.DESCRIPTION,
+        questions=[TextInput(title=strings.Queues.QueueAdd.Name.TITLE,
+                             description=strings.Queues.QueueAdd.Name.DESCRIPTION,
+                             name="name")],
+        back_link=BackLink(strings.Queues.QueueAdd.BACK, reverse_lazy("queues:queues")),
     )
 
 
 def edit_queue_form():
     return Form(
-        title=strings.Queues.QueueEdit.PAGE_HEADING,
-        questions=[TextInput(title=strings.Queues.QueueEdit.QUESTION_TITLE, name="name")],
-        back_link=BackLink("Back to queues", reverse_lazy("queues:queues")),
+        title=strings.Queues.QueueEdit.TITLE,
+        description=strings.Queues.QueueEdit.DESCRIPTION,
+        questions=[TextInput(title=strings.Queues.QueueEdit.Name.TITLE,
+                             description=strings.Queues.QueueEdit.Name.DESCRIPTION,
+                             name="name")],
+        back_link=BackLink(strings.Queues.QueueEdit.BACK, reverse_lazy("queues:queues")),
     )
