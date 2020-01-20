a0d2cc5496cf44fc6c6f52963fe1aaf2a418b77e
stephanieboyd <boydstep01@gmail.com>
2020-01-20 12:57:13 +0000

    Black formatting

--- cases/forms/finalise_case.py
+++ cases/forms/finalise_case.py
@@ -8,7 +8,11 @@ def approve_licence_form(case_id, standard):
     return Form(
         title=cases.FinaliseLicenceForm.APPROVE_TITLE,
         questions=[
-            DateInput(description=cases.FinaliseLicenceForm.DATE_DESCRIPTION, title=cases.FinaliseLicenceForm.DATE_TITLE, prefix=""),
+            DateInput(
+                description=cases.FinaliseLicenceForm.DATE_DESCRIPTION,
+                title=cases.FinaliseLicenceForm.DATE_TITLE,
+                prefix="",
+            ),
             TextInput(
                 name="duration",
                 description=cases.FinaliseLicenceForm.DURATION_DESCRIPTION,
@@ -17,7 +21,10 @@ def approve_licence_form(case_id, standard):
         ],
         back_link=conditional(
             standard,
-            BackLink(url=reverse_lazy("cases:final_advice_view", kwargs={"pk": case_id}), text=cases.FinaliseLicenceForm.Actions.BACK_TO_ADVICE_BUTTON),
+            BackLink(
+                url=reverse_lazy("cases:final_advice_view", kwargs={"pk": case_id}),
+                text=cases.FinaliseLicenceForm.Actions.BACK_TO_ADVICE_BUTTON,
+            ),
             BackLink(
                 url=reverse_lazy("cases:finalise_goods_countries", kwargs={"pk": case_id}),
                 text=cases.FinaliseLicenceForm.Actions.BACK_TO_DECISION_MATRIX_BUTTON,
@@ -36,7 +43,10 @@ def deny_licence_form(case_id, is_standard_licence):
         title=title,
         back_link=conditional(
             is_standard_licence,
-            BackLink(url=reverse_lazy("cases:final_advice_view", kwargs={"pk": case_id}), text=cases.FinaliseLicenceForm.Actions.BACK_TO_ADVICE_BUTTON),
+            BackLink(
+                url=reverse_lazy("cases:final_advice_view", kwargs={"pk": case_id}),
+                text=cases.FinaliseLicenceForm.Actions.BACK_TO_ADVICE_BUTTON,
+            ),
             BackLink(
                 url=reverse_lazy("cases:finalise_goods_countries", kwargs={"pk": case_id}),
                 text=cases.FinaliseLicenceForm.Actions.BACK_TO_DECISION_MATRIX_BUTTON,
