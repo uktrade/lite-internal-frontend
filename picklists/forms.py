234aea311d87beb1903a9216179b1f530512ff88
Jan Faracik <j.faracik@kainos.com>
2020-02-07 17:36:58 +0000

    Content and black

--- picklists/forms.py
+++ picklists/forms.py
@@ -1,24 +1,25 @@
-from lite_content.lite_internal_frontend import strings
 from django.urls import reverse_lazy
+
+from letter_templates.context_variables import get_sample_context_variables
+from lite_content.lite_internal_frontend import picklists
 from lite_forms.components import TextInput, TextArea, Form, Button, MarkdownArea, HiddenField, BackLink, HelpSection
 from lite_forms.helpers import conditional
 from lite_forms.styles import ButtonStyle
-
-from letter_templates.context_variables import get_sample_context_variables
 
 
 def add_picklist_item_form(request):
     picklist_type = request.GET.get("type")
 
     return Form(
-        title=getattr(strings.Picklist.Create, picklist_type.upper()),
+        title=getattr(picklists.NewPicklistForm, picklist_type.upper()),
         questions=[
-            TextInput(title="Name", name="name"),
+            TextInput(title=picklists.NewPicklistForm.Name.TITLE, name="name"),
             HiddenField("type", picklist_type),
-            TextArea(title="Text", name="text", extras={"max_length": 5000,}),
+            TextArea(title=picklists.NewPicklistForm.Text.TITLE, name="text", extras={"max_length": 5000,}),
         ],
-        back_link=BackLink("Back to picklists", reverse_lazy("picklists:picklists") + f"?type={picklist_type}"),
-        default_button_name="Save",
+        back_link=BackLink(
+            picklists.NewPicklistForm.BACK_LINK, reverse_lazy("picklists:picklists") + f"?type={picklist_type}"
+        ),
     )
 
 
@@ -58,37 +59,35 @@ def edit_picklist_item_form(picklist_item):
 
 def add_letter_paragraph_form(picklist_type):
     return Form(
-        title="Create a letter paragraph",
-        description="",
+        title=picklists.NewPicklistForm.LETTER_PARAGRAPH,
         questions=[
             HiddenField("type", picklist_type),
-            TextInput(title="Name", name="name", classes=["govuk-!-width-full"]),
+            TextInput(title=picklists.NewPicklistForm.Name.TITLE, name="name", classes=["govuk-!-width-full"]),
             MarkdownArea(
-                title="Paragraph Text",
+                title=picklists.NewPicklistForm.Text.TITLE,
                 name="text",
                 variables=get_sample_context_variables(),
                 extras={"max_length": 5000,},
             ),
         ],
-        helpers=[HelpSection("Help", None, "teams/markdown-help.html")],
+        helpers=[HelpSection(picklists.NewPicklistForm.HELP, None, "teams/markdown-help.html")],
     )
 
 
 def edit_letter_paragraph_form(picklist_item):
     return Form(
-        title="Edit " + picklist_item["name"],
-        description="",
+        title=picklists.NewPicklistForm.EDIT_PREFIX + " " + picklist_item["name"],
         questions=[
             HiddenField("type", picklist_item["type"]["key"]),
-            TextInput(title="Name", name="name", classes=["govuk-!-width-full"]),
+            TextInput(title=picklists.NewPicklistForm.Name.TITLE, name="name", classes=["govuk-!-width-full"]),
             MarkdownArea(
-                title="Paragraph Text",
+                title=picklists.NewPicklistForm.Text.TITLE,
                 name="text",
                 variables=get_sample_context_variables(),
                 extras={"max_length": 5000,},
             ),
         ],
-        helpers=[HelpSection("Help", None, "teams/markdown-help.html")],
+        helpers=[HelpSection(picklists.NewPicklistForm.HELP, None, "teams/markdown-help.html")],
     )
 
 
