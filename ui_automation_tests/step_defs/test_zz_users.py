2fde0e03a7bd61d7efcf0ad28d80ec5b78d9b6fb
asmund <asmund.bekker@gmail.com>
2020-02-14 10:42:19 +0000

    Fix e2e test header tag

--- ui_automation_tests/step_defs/test_zz_users.py
+++ ui_automation_tests/step_defs/test_zz_users.py
@@ -36,7 +36,7 @@ def test_manage_users(driver, open_internal_hub, context, internal_info):
     user_page.click_save_and_continue()
 
     assert (
-        driver.find_element_by_tag_name("h2").text == "Users"
+        driver.find_element_by_tag_name("h1").text == "Users"
     ), "Failed to return to Users list page after Adding user"
 
     assert utils.is_element_present(
