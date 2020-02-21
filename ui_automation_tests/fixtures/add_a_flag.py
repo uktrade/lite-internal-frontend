4f60148cd847a422e91237cb94780a0c0b43fb4e
Nick Philpott <nicholasp@kainos.com>
2020-02-21 15:50:53 +0000

    rename

--- ui_automation_tests/fixtures/add_a_flag.py
+++ ui_automation_tests/fixtures/add_a_flag.py
@@ -6,26 +6,26 @@ from shared.tools.utils import get_lite_client
 @fixture(scope="function")
 def add_case_flag(driver, context, api_client_config):
     lite_client = get_lite_client(context, api_client_config)
-    lite_client.flags.add_new_flag("Case " + utils.get_formatted_date_time_m_d_h_s(), "Case")
+    lite_client.flags.add_flag("Case " + utils.get_formatted_date_time_m_d_h_s(), "Case")
     context.flag_name = lite_client.context["flag_name"]
 
 
 @fixture(scope="function")
 def add_good_flag(driver, context, api_client_config):
     lite_client = get_lite_client(context, api_client_config)
-    lite_client.flags.add_new_flag("Good " + utils.get_formatted_date_time_m_d_h_s(), "Good")
+    lite_client.flags.add_flag("Good " + utils.get_formatted_date_time_m_d_h_s(), "Good")
     context.flag_name = lite_client.context["flag_name"]
 
 
 @fixture(scope="function")
 def add_destination_flag(driver, context, api_client_config):
     lite_client = get_lite_client(context, api_client_config)
-    lite_client.flags.add_new_flag("Place " + utils.get_formatted_date_time_m_d_h_s(), "Destination")
+    lite_client.flags.add_flag("Place " + utils.get_formatted_date_time_m_d_h_s(), "Destination")
     context.flag_name = lite_client.context["flag_name"]
 
 
 @fixture(scope="function")
 def add_organisation_flag(driver, context, api_client_config):
     lite_client = get_lite_client(context, api_client_config)
-    lite_client.flags.add_new_flag("Org " + utils.get_formatted_date_time_m_d_h_s(), "Organisation")
+    lite_client.flags.add_flag("Org " + utils.get_formatted_date_time_m_d_h_s(), "Organisation")
     context.flag_name = lite_client.context["flag_name"]
