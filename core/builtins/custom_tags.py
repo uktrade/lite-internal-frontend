771180486b8e749b4d9a0e868784fbccd952d76a
Andy Taylor <a.taylor@kainos.com>
2020-03-12 15:24:39 +0000

    undo change cause bandit

--- core/builtins/custom_tags.py
+++ core/builtins/custom_tags.py
@@ -199,10 +199,7 @@ def get_end_user(application: dict):
 
 @register.filter()
 def friendly_boolean_or_default_na(value):
-    if value is None:
-        return mark_safe(f'<span class="lite-hint">{strings.NOT_APPLICABLE}</span>')
-    else:
-        return friendly_boolean(value)
+    return default_na(value) if value is None else friendly_boolean(value)
 
 
 @register.filter()
