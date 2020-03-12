36c2ee9bee68ceaf2a9f416ec913cdf5e6c298b9
Jan Faracik <j.faracik@kainos.com>
2020-03-12 15:52:39 +0000

    Initial

--- templates/check_html_strings.py
+++ templates/check_html_strings.py
@@ -7,12 +7,12 @@ LCS_PATTERN = "{% lcs '(.*)' %}"
 
 def check_string_for_occurance(module, string):
     """
-	Check that the given string variable i.e. `CASES.Page.TITLE` is found within a given module (strings.py).
-	Checks each section of the path is found inside the module defined in the last iteration
-	:param module: Python module to search
-	:param string: Path to find (split into sections)
-	:return: True/False
-	"""
+    Check that the given string variable i.e. `CASES.Page.TITLE` is found within a given module (strings.py).
+    Checks each section of the path is found inside the module defined in the last iteration
+    :param module: Python module to search
+    :param string: Path to find (split into sections)
+    :return: True/False
+    """
     for path_section in string:
         module = module.__dict__.get(path_section)
         if not module:
@@ -23,20 +23,20 @@ def check_string_for_occurance(module, string):
 
 def get_strings_package(base_dir):
     """
-	Uses BASE_DIR to get the lite_content package
-	i.e. /Users/user/lite-internal-frontend = lite_internal_frontend
-	:return: strings package name
-	"""
+    Uses BASE_DIR to get the lite_content package
+    i.e. /Users/user/lite-internal-frontend = lite_internal_frontend
+    :return: strings package name
+    """
     project_name = os.path.basename(base_dir)
     return project_name.replace("-", "_", 2)
 
 
 def get_base_dir():
     """
-	Get's the BASE_DIR from settings.
-	Accounts for this file being called externally
-	:return: str BASE_DIR value
-	"""
+    Get's the BASE_DIR from settings.
+    Accounts for this file being called externally
+    :return: str BASE_DIR value
+    """
     try:
         from conf.settings import BASE_DIR
     except ModuleNotFoundError:
@@ -51,10 +51,10 @@ def get_base_dir():
 
 def get_all_lcs_strings(templates_folder):
     """
-	Get all LCS strings from the HTML files in the given folder
-	:param templates_folder: string path for the HTML folders
-	:return: set of found LCS strings
-	"""
+    Get all LCS strings from the HTML files in the given folder
+    :param templates_folder: string path for the HTML folders
+    :return: set of found LCS strings
+    """
     strings = set()
 
     for root, _, files in os.walk(templates_folder):
