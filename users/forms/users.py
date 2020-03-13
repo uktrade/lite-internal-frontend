f51f9eec6251e4fc15e7c30f9ce5faaf4313f4b0
Jan Faracik <j.faracik@kainos.com>
2020-03-13 10:43:14 +0000

    Update users.py

--- users/forms/users.py
+++ users/forms/users.py
@@ -12,17 +12,19 @@ def add_user_form(request):
     return Form(
         title=AddUserForm.TITLE,
         questions=[
-            TextInput(title=AddUserForm.Email.TITLE,
-                      description=AddUserForm.Email.DESCRIPTION,
-                      name="email"),
-            Select(title=AddUserForm.Team.TITLE,
-                   description=AddUserForm.Team.DESCRIPTION,
-                   name="team",
-                   options=get_teams(request, True)),
-            Select(title=AddUserForm.Role.TITLE,
-                   description=AddUserForm.Role.DESCRIPTION,
-                   name="role",
-                   options=get_roles(request, True)),
+            TextInput(title=AddUserForm.Email.TITLE, description=AddUserForm.Email.DESCRIPTION, name="email"),
+            Select(
+                title=AddUserForm.Team.TITLE,
+                description=AddUserForm.Team.DESCRIPTION,
+                name="team",
+                options=get_teams(request, True),
+            ),
+            Select(
+                title=AddUserForm.Role.TITLE,
+                description=AddUserForm.Role.DESCRIPTION,
+                name="role",
+                options=get_roles(request, True),
+            ),
         ],
         back_link=BackLink(AddUserForm.BACK_LINK, reverse_lazy("users:users")),
     )
@@ -32,21 +34,26 @@ def edit_user_form(request, user, can_edit_role: bool):
     return Form(
         title=EditUserForm.TITLE.format(user["first_name"], user["last_name"]),
         questions=[
-            TextInput(title=EditUserForm.Email.TITLE,
-                      description=EditUserForm.Email.DESCRIPTION,
-                      name="email"),
-            Select(title=EditUserForm.Team.TITLE,
-                   description=EditUserForm.Team.DESCRIPTION,
-                   name="team",
-                   options=get_teams(request, True)),
-            conditional(can_edit_role,
-                        Select(title=EditUserForm.Role.TITLE,
-                               description=EditUserForm.Role.DESCRIPTION,
-                               name="role",
-                               options=get_roles(request, True)),
-                        ),
+            TextInput(title=EditUserForm.Email.TITLE, description=EditUserForm.Email.DESCRIPTION, name="email"),
+            Select(
+                title=EditUserForm.Team.TITLE,
+                description=EditUserForm.Team.DESCRIPTION,
+                name="team",
+                options=get_teams(request, True),
+            ),
+            conditional(
+                can_edit_role,
+                Select(
+                    title=EditUserForm.Role.TITLE,
+                    description=EditUserForm.Role.DESCRIPTION,
+                    name="role",
+                    options=get_roles(request, True),
+                ),
+            ),
         ],
-        back_link=BackLink(EditUserForm.BACK_LINK.format(user["first_name"], user["last_name"]),
-                           reverse_lazy("users:user", kwargs={"pk": user["id"]})),
+        back_link=BackLink(
+            EditUserForm.BACK_LINK.format(user["first_name"], user["last_name"]),
+            reverse_lazy("users:user", kwargs={"pk": user["id"]}),
+        ),
         default_button_name=EditUserForm.SUBMIT_BUTTON,
     )
