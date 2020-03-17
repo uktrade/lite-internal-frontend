5e0a10d115d2fa36cb9a0ead93ff1ba523a15725
Jan Faracik <j.faracik@kainos.com>
2020-03-17 09:23:39 +0000

    Fix

--- cases/forms/review_goods_clc.py
+++ cases/forms/review_goods_clc.py
@@ -15,16 +15,16 @@ def review_goods_clc_query_form(request, back_url, is_goods_type):
                 title="Is this good controlled?",
                 name="is_good_controlled",
                 options=[
-                    Option(key=conditional(is_goods_type, "True", "yes"), value="Yes", show_pane="pane_control_code"),
+                    Option(key=conditional(is_goods_type, "True", "yes"), value="Yes", components=[
+                        control_list_entry_question(
+                            control_list_entries=get_control_list_entries(None, convert_to_options=True),
+                            title="What is the correct control list entry?",
+                            name="control_code",
+                            inset_text=False,
+                        ),
+                    ]),
                     Option(key=conditional(is_goods_type, "False", "no"), value="No"),
                 ],
-                classes=["govuk-radios--inline"],
-            ),
-            control_list_entry_question(
-                control_list_entries=get_control_list_entries(None, convert_to_options=True),
-                title="What is the correct control list entry?",
-                name="control_code",
-                inset_text=False,
             ),
             RadioButtons(
                 title="Which report summary would you like to use? (optional)",
@@ -33,7 +33,7 @@ def review_goods_clc_query_form(request, back_url, is_goods_type):
                 description="You only need to do this if the item is controlled",
                 classes=["test"],
             ),
-            TextArea(title="Good's comment (optional)", name="comment", extras={"max_length": 500,}),
+            TextArea(title="Good's comment", name="comment", extras={"max_length": 500,}, optional=True),
         ],
         default_button_name=cases.ReviewGoodsForm.CONFIRM_BUTTON,
         back_link=BackLink(cases.ReviewGoodsForm.BACK_LINK, back_url),
