89708bdc1fff92a2c39ab03443784918b4fccc06
BenAndrew-Kainos <bena@kainos.com>
2020-03-18 15:05:29 +0000

    Move strings to lite content

--- cases/forms/done_with_case.py
+++ cases/forms/done_with_case.py
@@ -1,4 +1,5 @@
 from cases.services import get_user_case_queues
+from lite_content.lite_internal_frontend.cases import DoneWithCaseOnQueueForm
 from lite_forms.components import Form, Option, Checkboxes
 
 
@@ -6,15 +7,15 @@ def done_with_case_form(request, case_pk):
     queues, _ = get_user_case_queues(request, case_pk)
 
     return Form(
-        title="Unassign queues",
+        title=DoneWithCaseOnQueueForm.TITLE,
         questions=[
             Checkboxes(
                 name="queues[]",
                 options=[Option(queue["id"], queue["name"]) for queue in queues["queues"]],
-                title="",
-                description="Select which queues you are done with this case on",
+                title=DoneWithCaseOnQueueForm.CHECKBOX_TITLE,
+                description=DoneWithCaseOnQueueForm.CHECKBOX_DESCRIPTION,
                 optional=False,
             )
         ],
-        default_button_name="Submit",
+        default_button_name=DoneWithCaseOnQueueForm.SUBMIT,
     )
