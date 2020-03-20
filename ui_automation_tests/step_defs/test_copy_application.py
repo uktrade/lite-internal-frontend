049215f5942db59305e941eb5d009e21597e6ba6
Andy Taylor <a.taylor@kainos.com>
2020-03-20 14:57:36 +0000

    feat: LT-2274 and LT-2275 (#411)

--- ui_automation_tests/step_defs/test_copy_application.py
+++ ui_automation_tests/step_defs/test_copy_application.py
@@ -14,8 +14,14 @@ def original_application_linked(driver, context):
 @given("I have an open application from copying")  # noqa
 def copy_open_app(driver, apply_for_open_application, api_client_config, context):  # noqa
     lite_client = get_lite_client(context, api_client_config)  # noqa
-    data = {"name": "new application"}
-    lite_client.applications.add_copied_application(context.app_id, data)
+    app_name = {"name": "new application"}
+    end_use_details = {
+        "intended_end_use": "intended end use",
+        "is_military_end_use_controls": False,
+        "is_informed_wmd": False,
+        "is_suspected_wmd": False,
+    }
+    lite_client.applications.add_copied_open_application(context.app_id, app_name, end_use_details)
     context.old_app_id = context.app_id
     context.app_id = lite_client.context["application_id"]
     context.case_id = lite_client.context["application_id"]
