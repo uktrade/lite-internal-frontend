430109cf9acca246220212313c7b02930ca1aff5
Jan Faracik <j.faracik@kainos.com>
2020-03-31 12:04:09 +0100

    Cleanup

--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 
+from conf.constants import ALL_CASES_QUEUE_ID
 from core import views
+from queues import views as queues_views
 
 app_name = "core"
 
 urlpatterns = [
-    path("", views.Index.as_view(), name="index"),
+    path("", queues_views.Cases.as_view(), name="index", kwargs={"queue_pk": ALL_CASES_QUEUE_ID}),
     path("menu/", views.menu, name="menu"),
 ]
--- core/views.py
+++ core/views.py
@@ -1,10 +1,4 @@
 from django.shortcuts import render
-from django.urls import reverse_lazy
-from django.views.generic import RedirectView
-
-
-class Index(RedirectView):
-    url = reverse_lazy("queues:cases")
 
 
 def menu(request):
