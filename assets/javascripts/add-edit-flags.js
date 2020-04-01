a3b7f20d288e88d6960b05f0103f9a3ffde800ed
Jan Faracik <j.faracik@kainos.com>
2020-04-01 14:20:56 +0100

    Update add-edit-flags.js

--- assets/javascripts/add-edit-flags.js
+++ assets/javascripts/add-edit-flags.js
@@ -5,3 +5,17 @@ tippy('.govuk-radios__input', {
 });
 
 $("#pane_label").addClass("govuk-inset-text");
+
+$('input[type=radio]').change(function() {
+    updateLabelVisibility();
+});
+
+function updateLabelVisibility() {
+    if ($('.govuk-radios__input:checked').val() == "default") {
+        $("#pane_label").hide();
+    } else {
+        $("#pane_label").show();
+    }
+}
+
+updateLabelVisibility();
