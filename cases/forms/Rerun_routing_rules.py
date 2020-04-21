10c594ba9e52b12af9225fff616326a3099a3972
Mark Spiers <marksp@kainos.com>
2020-04-21 12:54:42 +0100

    lite-content added

--- cases/forms/Rerun_routing_rules.py
+++ cases/forms/Rerun_routing_rules.py
@@ -1,13 +1,14 @@
 from lite_forms.generators import confirm_form
+from lite_content.lite_internal_frontend.cases import Manage
 
 
 def rerun_routing_rules_confirmation_form():
     return confirm_form(
-        title="Do you want to rerun routing rules?",
+        title=Manage.RerunRoutingRules.TITLE,
         confirmation_name="confirm",
-        back_link_text="Back to case",
+        back_link_text=Manage.RerunRoutingRules.BACKLINK,
         back_url="#",
-        yes_label="Yes, rerun",
-        no_label="Cancel",
-        submit_button_text="Continue",
+        yes_label=Manage.RerunRoutingRules.YES,
+        no_label=Manage.RerunRoutingRules.NO,
+        submit_button_text=Manage.RerunRoutingRules.SUBMIT_BUTTON,
     )
