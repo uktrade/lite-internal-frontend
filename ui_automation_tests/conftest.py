6067088cbf6290b61bec28954a920b057496445f
asmund <asmund.bekker@gmail.com>
2020-05-04 15:32:19 +0100

    lint

--- ui_automation_tests/conftest.py
+++ ui_automation_tests/conftest.py
@@ -457,27 +457,27 @@ def assert_flag_is_assigned(driver, context):  # noqa
     assert Shared(driver).is_flag_applied(context.flag_name)
 
 
-@when("I show filters")
-def show_filters(driver):
+@when("I show filters")   # noqa
+def show_filters(driver):  # noqa
     ApplicationPage(driver).click_show_filters_link()
 
 
 @when(parsers.parse('filter user_type has been changed to "{user_type}"'))  # noqa
-def filter_status_change(driver, user_type):
+def filter_status_change(driver, user_type):  # noqa
     page = ApplicationPage(driver)
     page.select_filter_user_type_from_dropdown(user_type)
     page.click_apply_filters_button()
 
 
-@then("exporter is at the first audit in the trail")
-def exporter_first_audit_in_trail(driver, exporter_info):
+@then("exporter is at the first audit in the trail")  # noqa
+def exporter_first_audit_in_trail(driver, exporter_info):  # noqa
     first_audit = ApplicationPage(driver).get_text_of_first_audit_trail_item()
     assert f"{exporter_info['first_name']}" in first_audit
     assert f"{exporter_info['last_name']}" in first_audit
 
 
-@then("exporter is not in the audit trail")
-def exporter_is_not_in_audit_trail(driver, exporter_info):
+@then("exporter is not in the audit trail")  # noqa
+def exporter_is_not_in_audit_trail(driver, exporter_info):  # noqa
     audits = ApplicationPage(driver).get_audit_elements()
     for audit in audits:
         assert f"{exporter_info['first_name']}" not in audit
