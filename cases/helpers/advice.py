a08e0bd43e204fd9515d599b91398c79b441ec21
Jan Faracik <j.faracik@kainos.com>
2020-05-11 20:32:16 +0100

    Update advice.py

--- cases/helpers/advice.py
+++ cases/helpers/advice.py
@@ -62,10 +62,7 @@ def get_advice_additional_context(request, case, permissions):
     ):
         current_advice_level = "final"
 
-    if (
-        not _can_user_create_and_edit_advice(case, permissions)
-        or status_props["is_terminal"]
-    ):
+    if not _can_user_create_and_edit_advice(case, permissions) or status_props["is_terminal"]:
         current_advice_level = None
 
     return {
