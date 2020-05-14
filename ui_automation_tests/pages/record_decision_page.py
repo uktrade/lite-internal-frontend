323e77b405483f3b321cbb9f568bff3601fd1447
Nick Philpott <nicholasp@kainos.com>
2020-05-14 11:58:37 +0100

    fixx?

--- ui_automation_tests/pages/record_decision_page.py
+++ ui_automation_tests/pages/record_decision_page.py
@@ -1,6 +1,8 @@
 from shared.BasePage import BasePage
+from shared.tools.helpers import scroll_to_element_by_id
 
 
 class RecordDecision(BasePage):
     def click_on_decision_number(self, no):
+        scroll_to_element_by_id(self.driver, no)
         self.driver.find_element_by_id(no).click()
