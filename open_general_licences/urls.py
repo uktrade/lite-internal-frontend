bfde3011bdb809d925d3e09ca042c9fe7d861da4
Jan Faracik <j.faracik@kainos.com>
2020-05-20 13:54:19 +0100

    Total frustration, detioration

--- open_general_licences/urls.py
+++ open_general_licences/urls.py
@@ -7,7 +7,7 @@ app_name = "open_general_licences"
 urlpatterns = [
     path("", views.ListView.as_view(), name="open_general_licences"),
     path("<uuid:pk>/", views.DetailView.as_view(), name="open_general_licence"),
-    path("<uuid:pk>/edit/", views.UpdateView.as_view(), name="edit"),
+    path("<uuid:pk>/edit-<str:edit>/", views.UpdateView.as_view(), name="edit"),
     path("<uuid:pk>/<str:status>/", views.ChangeStatusView.as_view(), name="change_status"),
     path("create/", views.CreateView.as_view(), name="create"),
 ]
