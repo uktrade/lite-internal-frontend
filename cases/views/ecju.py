1960f1750fc445f750d563674cfe511024c9ea95
Jan Faracik <j.faracik@kainos.com>
2020-05-22 16:01:49 +0100

    Update ecju.py

--- cases/views/ecju.py
+++ cases/views/ecju.py
@@ -2,7 +2,7 @@ from django.urls import reverse
 
 from cases.forms.create_ecju_query import new_ecju_query_form
 from cases.services import get_case, post_ecju_query
-from lite_forms.views import MultiFormView, SingleFormView
+from lite_forms.views import SingleFormView
 
 
 class NewECJUQueryView(SingleFormView):
