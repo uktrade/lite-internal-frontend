fbe6a0fb18c6fe90503fcd3f2cd5da609039e9a0
rorysproule1 <52039706+rorysproule1@users.noreply.github.com>
2020-05-26 16:44:58 +0100

    Updated js

--- assets/javascripts/advice.js
+++ assets/javascripts/advice.js
@@ -2,11 +2,17 @@ let text_for = "text";
 let text_label = $('label[for=' + text_for + ']');
 
 $( document ).ready(function() {
-	text_label.append('<span class="lite-form-optional">(optional)</span>')
+	text_label.append('<span class="lite-form-optional">(optional)</span>');
+	addOrRemoveOptional()
+
 });
 
 $('.govuk-radios--conditional').on('click', function() {
+	addOrRemoveOptional()
 
+});
+
+function addOrRemoveOptional() {
 	let advice_decision = $('input:radio[id="type-refuse"]:checked').val();
 
 	// if the advice decision isn't 'Refuse', add (optional) to the reason for decision title
@@ -17,5 +23,4 @@ $('.govuk-radios--conditional').on('click', function() {
 	if (advice_decision === 'refuse') {
 		text_label.children().remove();
 	}
-
-});
+}
