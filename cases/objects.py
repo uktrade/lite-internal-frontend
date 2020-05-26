88153dd56ccb0421bddf474c4fedf4951599f8e1
stephanieboyd <s.boyd@kainos.com>
2020-05-26 13:38:49 +0100

    fix logic

--- cases/objects.py
+++ cases/objects.py
@@ -65,8 +65,12 @@ class Case(Munch):
     def open_app_parties(self):
         parties = []
         if self.data["case_type"]["sub_type"]["key"] == CaseType.OPEN.value:
-            parties.append(self.data.get("end_user"))
-            if self.data["goodstype_category"]["key"] == GoodsTypeCategory.MILITARY:
+            if self.data.get("end_user"):
+                parties.append(self.data.get("end_user"))
+            if (
+                self.data.get("ultimate_end_users")
+                and self.data["goodstype_category"]["key"] == GoodsTypeCategory.MILITARY
+            ):
                 parties.extend(self.data.get("ultimate_end_users"))
 
         return parties
