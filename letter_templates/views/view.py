1931f36fe75e4ab5f1cd794adb8979fb5e84c1fb
BenAndrew-Kainos <bena@kainos.com>
2020-06-05 15:54:18 +0100

    review comment

--- letter_templates/views/view.py
+++ letter_templates/views/view.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 
-from core.helpers import convert_dict_to_query_params
+from core.helpers import convert_dict_to_query_params, convert_parameters_to_query_params
 from letter_templates.services import (
     get_letter_templates,
     get_letter_template,
@@ -11,7 +11,7 @@ from letter_templates.services import (
 class LetterTemplatesList(TemplateView):
     def get(self, request, **kwargs):
         params = {"page": int(request.GET.get("page", 1))}
-        data, _ = get_letter_templates(request, convert_dict_to_query_params(params))
+        data, _ = get_letter_templates(request, convert_parameters_to_query_params(params))
         context = {"data": data, "page": params.pop("page")}
         return render(request, "letter-templates/letter-templates.html", context)
 
