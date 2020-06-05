736288bcdb4df2fe49bf018df9cc7a65d067f176
BenAndrew-Kainos <bena@kainos.com>
2020-06-05 15:15:43 +0100

    Filter by template name

--- letter_templates/views/view.py
+++ letter_templates/views/view.py
@@ -6,13 +6,21 @@ from letter_templates.services import (
     get_letter_templates,
     get_letter_template,
 )
+from lite_forms.components import FiltersBar, TextInput
 
 
 class LetterTemplatesList(TemplateView):
     def get(self, request, **kwargs):
         params = {"page": int(request.GET.get("page", 1))}
+        name = request.GET.get("name")
+        if name:
+            params["name"] = name
+
         data, _ = get_letter_templates(request, convert_dict_to_query_params(params))
-        context = {"data": data, "page": params.pop("page")}
+        filters = FiltersBar(
+            [TextInput(name="name", title="name"), ]
+        )
+        context = {"data": data, "page": params.pop("page"), "filters": filters,}
         return render(request, "letter-templates/letter-templates.html", context)
 
 
