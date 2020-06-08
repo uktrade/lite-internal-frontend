364db342240bb7676ea741854d106b115bcc3b34
Jan Faracik <janf@kainos.com>
2020-06-08 15:12:46 +0100

    Update create_ecju_query.py

--- cases/forms/create_ecju_query.py
+++ cases/forms/create_ecju_query.py
@@ -1,5 +1,3 @@
-from django.urls import reverse
-
 from core.components import PicklistPicker
 from lite_content.lite_internal_frontend.cases import EcjuQueries
 from lite_content.lite_internal_frontend.strings import cases
