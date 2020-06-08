76e8bb03138b3beb0a42ad56e89bbe6295d56e8b
BenAndrew-Kainos <bena@kainos.com>
2020-06-08 09:39:39 +0100

    linting

--- letter_templates/views/view.py
+++ letter_templates/views/view.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 
-from core.helpers import convert_dict_to_query_params, convert_parameters_to_query_params
+from core.helpers import convert_dict_to_query_params
 from letter_templates.services import (
     get_letter_templates,
     get_letter_template,
