3b3557d970a620a1618b0261019a22d9f43e0de4
Jan Faracik <janf@kainos.com>
2020-06-15 10:49:10 +0100

    stuff

--- core/builtins/custom_tags.py
+++ core/builtins/custom_tags.py
@@ -278,7 +278,7 @@ def linkify(address, name=None):
     address = escape(address)
     name = escape(name)
 
-    return safe(f'<a href="{address}" class="govuk-link govuk-link--no-visited-state">{name}</a>')
+    return safe(f'<a href="{address}" rel="noreferrer noopener" target="_blank" class="govuk-link govuk-link--no-visited-state">{name} <span class="govuk-visually-hidden">(opens in new tab)</span></a>')
 
 
 @register.filter
