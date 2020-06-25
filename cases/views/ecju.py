d226b7ad6fe8f2ea7a26b81861fa4e5f8f4ec7b3
Mark Spiers <marksp@kainos.com>
2020-06-25 16:02:49 +0100

    ecju query updated for compliance specifically

--- cases/views/ecju.py
+++ cases/views/ecju.py
@@ -1,15 +1,17 @@
 from django.urls import reverse
 
-from cases.forms.create_ecju_query import new_ecju_query_form
+from cases.forms.create_ecju_query import new_ecju_query_form, ECJUQueryTypes
 from cases.services import get_case, post_ecju_query
+from lite_forms.components import HiddenField
 from lite_forms.views import SingleFormView
 
 
 class NewECJUQueryView(SingleFormView):
     def init(self, request, **kwargs):
+        query_type = request.GET.get("query_type", ECJUQueryTypes.ECJU_QUERY)
         self.object_pk = kwargs["pk"]
         self.context = {"case": get_case(request, self.object_pk)}
-        self.form = new_ecju_query_form(kwargs["pk"], self.object_pk)
+        self.form = new_ecju_query_form(kwargs["pk"], self.object_pk, query_type)
         self.action = post_ecju_query
         self.success_message = "ECJU query sent successfully"
         self.success_url = reverse(
