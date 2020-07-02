5741d62f795f6c1c2eed50d155782ff7346e796b
Jan Faracik <j.faracik@kainos.com>
2020-07-02 15:21:22 +0100

    test

--- cases/urls.py
+++ cases/urls.py
@@ -64,8 +64,6 @@ urlpatterns = [
     path("assign-user/", main.UserWorkQueue.as_view(), name="assign_user"),
     path("assign-user-queue/<uuid:user_pk>/", main.UserTeamQueue.as_view(), name="assign_user_queue"),
     path("rerun-routing-rules/", main.RerunRoutingRules.as_view(), name="rerun_routing_rules",),
-    path("good/<uuid:good_pk>/", main.Good.as_view(), name="good"),
-    path("destination/<uuid:destination_pk>/", main.Destination.as_view(), name="destination"),
     # Compliance
     path("create-visit-report/", compliance.CreateVisitReport.as_view(), name="create_visit_report"),
     path("visit-report/", compliance.VisitReportDetails.as_view(), name="visit_report"),
