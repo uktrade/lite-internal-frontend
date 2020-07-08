284f3b961b0b7a22c3251ef551175fdf2b80469a
Nick Philpott <nicholasp@kainos.com>
2020-07-08 14:14:47 +0100

    one pound fix

--- ui_automation_tests/pages/BasePage.py
+++ ui_automation_tests/pages/BasePage.py
@@ -1,7 +1,9 @@
+from selenium.webdriver.common.by import By
 from selenium.webdriver.remote.webdriver import WebDriver
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.wait import WebDriverWait
 
 from ui_automation_tests.shared import functions
-from ui_automation_tests.shared.tools.wait import wait_for_element
 
 
 class BasePage:
@@ -10,7 +12,9 @@ class BasePage:
 
         # Wait for the cases list to load before interacting with the page
         if functions.element_with_id_exists(self.driver, "link-queue"):
-            wait_for_element(driver, "text-case-count")
+            WebDriverWait(driver, 10).until(
+                expected_conditions.visibility_of_element_located((By.ID, "text-case-count"))
+            )
 
         # The case header is sticky and can often overlay elements preventing clicks,
         # therefore disable the stickyness of the header when running tests
