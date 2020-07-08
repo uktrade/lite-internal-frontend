64754bbea8ac0b0883a81c71274ec062af139bb1
Nick Philpott <nicholasp@kainos.com>
2020-07-08 12:15:32 +0100

    base page

--- ui_automation_tests/pages/BasePage.py
+++ ui_automation_tests/pages/BasePage.py
@@ -1,7 +1,4 @@
-from selenium.webdriver.common.by import By
 from selenium.webdriver.remote.webdriver import WebDriver
-from selenium.webdriver.support import expected_conditions
-from selenium.webdriver.support.wait import WebDriverWait
 
 from ui_automation_tests.shared import functions
 from ui_automation_tests.shared.tools.wait import wait_until_page_is_loaded
