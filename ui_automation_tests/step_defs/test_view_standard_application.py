b1e9f52476ea229871a31c06f7dfb582994a5e47
Chris Iliev <c.iliev@kainos.com>
2020-07-09 15:36:46 +0100

    e2e tidy up

--- ui_automation_tests/step_defs/test_view_standard_application.py
+++ ui_automation_tests/step_defs/test_view_standard_application.py
@@ -54,15 +54,10 @@ def i_see_destinations(driver, context):
 def i_see_good_details_view_link(driver, context):  # noqa
     goods = CasePage(driver).get_goods()
     for good in goods:
-        LINK_ID = "view-good-details"
-        # selector = "td[id='" + LINK_ID + "']"
-        # print(selector)
-        view_link_table_cell = good.find_element_by_css_selector("td#view-good-details")
-        # view_link_table_cell = good.find_elements_by_tag_name("a")
-        print(view_link_table_cell)
-
+        view_link_table_cell = good.find_element_by_css_selector("td#view-good-details a")
         if view_link_table_cell:
-            print(view_link_table_cell.text)
+            print("view_link_table_cell.text")
+            assert "View" in view_link_table_cell.text
 
 
 @then("I see an inactive party on page")
