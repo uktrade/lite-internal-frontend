a1262ed59afdc585f222976b7e2f99958f5319c6
Jan Faracik <j.faracik@kainos.com>
2020-07-16 16:25:56 +0100

    Update unassign_queue_page.py

--- ui_automation_tests/pages/unassign_queue_page.py
+++ ui_automation_tests/pages/unassign_queue_page.py
@@ -6,3 +6,6 @@ class UnassignQueuePage(BasePage):
     def check_all_checkboxes(self):
         for checkbox in self.driver.find_elements_by_css_selector(selectors.CHECKBOX):
             checkbox.click()
+
+    def check_unassign_checkbox(self, queue_name):
+        self.driver.find_element_by_id(queue_name).click()
